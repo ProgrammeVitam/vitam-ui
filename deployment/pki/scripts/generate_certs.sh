e8a2a88e85321ec491cd69de60b7777a9e97ec99
Mathieu Leguay <mathieu.leguay@teamdlab.com>
2020-04-23 00:15:17 +0200

    [DLAB-4126] Upgrade deployment (PKI & Ansible scripts)
    - Upgrade functions.sh: 
    Addition of genericity on the functions allowing to handle a vault file
    - Add ERASE as input parameters on PKI's scripts in order to preverse existing items when we execute scripts (add module, clients ...)
    - Upgrade generate_stores.sh in order to populate dynamically the linked vault file
    - Upgrade packaging in order to add the archive of deployment
    - Upgrade scripts according to the PKI in order to clean/fix deployment for keystores/trustores and linked configuration

--- deployment/pki/scripts/generate_certs.sh
+++ deployment/pki/scripts/generate_certs.sh
@@ -11,6 +11,13 @@ set -e
 ############################# Functions ##############################
 ######################################################################
 
+# Génération du chemin d'un certificat serveur
+function getHostCertificatePath {
+    local TYPE_CERTIFICAT="${1}"
+    local HOSTNAME="${2}"
+    echo "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}"
+}
+
 # Génération d'un certificat serveur
 function generateHostCertificate {
     local COMPOSANT="${1}"
@@ -29,12 +36,13 @@ function generateHostCertificate {
     export OPENSSL_CRT_DIR=${TYPE_CERTIFICAT}
 
     pki_logger "Création du certificat ${TYPE_CERTIFICAT} pour ${COMPOSANT} hébergé sur ${HOSTNAME}..."
-    mkdir -p "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}"
+    local HOST_CERTIFICATE_PATH=$(getHostCertificatePath ${TYPE_CERTIFICAT} ${HOSTNAME})
+    mkdir -p "${HOST_CERTIFICATE_PATH}"
     pki_logger "Generation de la clé..."
     openssl req -newkey "${PARAM_KEY_CHIFFREMENT}" \
         -passout pass:"${CERT_KEY}" \
-        -keyout "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}/${COMPOSANT}.key" \
-        -out "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}/${COMPOSANT}.req" \
+        -keyout "${HOST_CERTIFICATE_PATH}/${COMPOSANT}.key" \
+        -out "${HOST_CERTIFICATE_PATH}/${COMPOSANT}.req" \
         -nodes \
         -config "${REPERTOIRE_CONFIG}/crt-config" \
         -batch
@@ -42,16 +50,23 @@ function generateHostCertificate {
     pki_logger "Generation du certificat signé avec CA ${TYPE_CERTIFICAT}..."
     openssl ca -config "${REPERTOIRE_CONFIG}/crt-config" \
         -passin pass:"${INTERMEDIATE_CA_KEY}" \
-        -out "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}/${COMPOSANT}.crt" \
-        -in "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}/${COMPOSANT}.req" -batch
+        -out "${HOST_CERTIFICATE_PATH}/${COMPOSANT}.crt" \
+        -in "${HOST_CERTIFICATE_PATH}/${COMPOSANT}.req" -batch
        # -extensions extension_${TYPE_CERTIFICAT} -batch
 
     openssl x509 \
-        -in "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}/${COMPOSANT}.crt" \
-        -out "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}/${COMPOSANT}.pem"
+        -in "${HOST_CERTIFICATE_PATH}/${COMPOSANT}.crt" \
+        -out "${HOST_CERTIFICATE_PATH}/${COMPOSANT}.pem"
 
-    purge_directory "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/hosts/${HOSTNAME}"
+    purge_directory "${HOST_CERTIFICATE_PATH}"
     purge_directory "${REPERTOIRE_CONFIG}/${TYPE_CERTIFICAT}"
+}
+
+# Génération du chemin d'un certificat de timestamping
+function getTimestampCertificatePath {
+    local TYPE_CERTIFICAT="${1}"
+    local HOSTNAME="${2}"
+    echo "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/vitam"
 }
 
 # Génération d'un certificat de timestamping ; le nom du certificat est dérivé de son usage
@@ -59,8 +74,8 @@ function generateTimestampCertificate {
     local USAGE="${1}"
     local CERT_KEY="${2}"
     local INTERMEDIATE_CA_KEY="${3}"
+    local TYPE_CERTIFICAT="${4}"
     local CN_VALEUR="${USAGE}"
-    local TYPE_CERTIFICAT="timestamping"
 
     # Correctly set certificate CN (env var is read inside the openssl configuration file)
     export OPENSSL_CN="${CN_VALEUR}"
@@ -68,12 +83,13 @@ function generateTimestampCertificate {
     export OPENSSL_CRT_DIR=${TYPE_CERTIFICAT}
 
     pki_logger "Création du certificat ${TYPE_CERTIFICAT} pour usage ${USAGE}"
-    mkdir -p "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/vitam"
+    local TIMESTAMP_CERTIFICATE_PATH=$(getTimestampCertificatePath ${TYPE_CERTIFICAT})
+    mkdir -p "${TIMESTAMP_CERTIFICATE_PATH}"
     pki_logger "Generation de la clé..."
     openssl req -newkey "${PARAM_KEY_CHIFFREMENT}" \
         -passout pass:"${CERT_KEY}" \
-        -keyout "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/vitam/${USAGE}.key" \
-        -out "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/vitam/${USAGE}.req" \
+        -keyout "${TIMESTAMP_CERTIFICATE_PATH}/${USAGE}.key" \
+        -out "${TIMESTAMP_CERTIFICATE_PATH}/${USAGE}.req" \
         -nodes \
         -config "${REPERTOIRE_CONFIG}/crt-config" \
         -batch
@@ -81,12 +97,20 @@ function generateTimestampCertificate {
     pki_logger "Generation du certificat signé avec CA ${TYPE_CERTIFICAT}..."
     openssl ca -config "${REPERTOIRE_CONFIG}/crt-config" \
         -passin pass:"${INTERMEDIATE_CA_KEY}" \
-        -out "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/vitam/${USAGE}.crt" \
-        -in "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/vitam/${USAGE}.req" \
+        -out "${TIMESTAMP_CERTIFICATE_PATH}/${USAGE}.crt" \
+        -in "${TIMESTAMP_CERTIFICATE_PATH}/${USAGE}.req" \
         -extensions extension_${TYPE_CERTIFICAT} -batch
 
-    purge_directory "${REPERTOIRE_CERTIFICAT}/${TYPE_CERTIFICAT}/vitam"
+    purge_directory "${TIMESTAMP_CERTIFICATE_PATH}"
     purge_directory "${REPERTOIRE_CONFIG}/${TYPE_CERTIFICAT}"
+}
+
+
+# Génération du chemin d'un certificat client
+function getClientCertificatePath {
+    local CLIENT_TYPE="${1}"
+    local CLIENT_NAME="${2}"
+    echo "${REPERTOIRE_CERTIFICAT}/${CLIENT_TYPE}/clients/${CLIENT_NAME}"
 }
 
 # Génération d'un certificat client
@@ -103,23 +127,24 @@ function generateClientCertificate {
     export OPENSSL_CRT_DIR=${CLIENT_TYPE}
 
     pki_logger "Création du certificat ${TYPE_CERTIFICAT} pour ${CLIENT_NAME}"
-    mkdir -p "${REPERTOIRE_CERTIFICAT}/${CLIENT_TYPE}/clients/${CLIENT_NAME}"
+    local CLIENT_CERTIFICATE_PATH=$(getClientCertificatePath ${CLIENT_TYPE} ${CLIENT_NAME})
+    mkdir -p "${CLIENT_CERTIFICATE_PATH}"
     pki_logger "Generation de la clé..."
     openssl req -newkey "${PARAM_KEY_CHIFFREMENT}" \
         -passout pass:"${MDP_KEY}" \
-        -keyout "${REPERTOIRE_CERTIFICAT}/${CLIENT_TYPE}/clients/${CLIENT_NAME}/${CLIENT_NAME}.key" \
-        -out "${REPERTOIRE_CERTIFICAT}/${CLIENT_TYPE}/clients/${CLIENT_NAME}/${CLIENT_NAME}.req" \
+        -keyout "${CLIENT_CERTIFICATE_PATH}/${CLIENT_NAME}.key" \
+        -out "${CLIENT_CERTIFICATE_PATH}/${CLIENT_NAME}.req" \
         -config "${REPERTOIRE_CONFIG}/crt-config" \
         -batch
 
     pki_logger "Generation du certificat signé avec ${CLIENT_TYPE}..."
     openssl ca -config "${REPERTOIRE_CONFIG}/crt-config" \
         -passin pass:"${MDP_CAINTERMEDIATE_KEY}" \
-        -out "${REPERTOIRE_CERTIFICAT}/${CLIENT_TYPE}/clients/${CLIENT_NAME}/${CLIENT_NAME}.crt" \
-        -in "${REPERTOIRE_CERTIFICAT}/${CLIENT_TYPE}/clients/${CLIENT_NAME}/${CLIENT_NAME}.req" \
+        -out "${CLIENT_CERTIFICATE_PATH}/${CLIENT_NAME}.crt" \
+        -in "${CLIENT_CERTIFICATE_PATH}/${CLIENT_NAME}.req" \
         -extensions extension_${TYPE_CERTIFICAT} -batch
 
-    purge_directory "${REPERTOIRE_CERTIFICAT}/${CLIENT_TYPE}/clients/${CLIENT_NAME}"
+    purge_directory "${CLIENT_CERTIFICATE_PATH}"
     purge_directory "${REPERTOIRE_CONFIG}/${CLIENT_TYPE}"
 }
 
@@ -133,19 +158,25 @@ function generateHostCertAndStorePassphrase {
 
     # sed "1 d" : remove the first line
     for SERVER in $(ansible -i ${ENVIRONNEMENT_FILE} --list-hosts ${HOSTS_GROUP} ${ANSIBLE_VAULT_PASSWD}| sed "1 d"); do
-        # Generate the key
-        local CERT_KEY=$(generatePassphrase)
-        # Create the certificate
-        generateHostCertificate ${COMPONENT} \
-                                ${CERT_KEY} \
-                                ${CA_INTERMEDIATE_PASSWORD} \
-                                ${SERVER} \
-                                "server" \
-                                "${COMPONENT}.service.${CONSUL_DOMAIN}" \
-                                "${COMPONENT}.service.${CONSUL_DOMAIN}"
-        # Store the key to the vault
-        setComponentPassphrase certs "server_${COMPONENT}_key" \
-                                     "${CERT_KEY}"
+        
+        local SERVER_CERTIFICATE_PATH=$(getHostCertificatePath "server" ${SERVER})
+        if [ ! -f "${SERVER_CERTIFICATE_PATH}/${COMPONENT}.crt" ]; then
+            # Generate the key
+            local CERT_KEY=$(generatePassphrase)
+            # Create the certificate
+            generateHostCertificate ${COMPONENT} \
+                                    ${CERT_KEY} \
+                                    ${CA_INTERMEDIATE_PASSWORD} \
+                                    ${SERVER} \
+                                    "server" \
+                                    "${COMPONENT}.service.${CONSUL_DOMAIN}" \
+                                    "${COMPONENT}.service.${CONSUL_DOMAIN}"
+            # Store the key to the vault
+            setComponentPassphrase certs "server_${COMPONENT}_key" \
+                                        "${CERT_KEY}"
+        else
+            pki_logger "Le certificat SERVER - ${SERVER} - ${COMPONENT}.crt existe déjà. Il ne sera pas recrée..."
+        fi
     done
 }
 
@@ -155,16 +186,22 @@ function generateTimestampCertAndStorePassphrase {
 
     # Récupération du password de la CA_INTERMEDIATE dans le vault-ca
     CA_INTERMEDIATE_PASSWORD=$(getComponentPassphrase ca "ca_intermediate_timestamping")
-
-    # Generate the key
-    local CERT_KEY=$(generatePassphrase)
-    # Create the certificate
-    generateTimestampCertificate ${USAGE} \
-                                 ${CERT_KEY} \
-                                 ${CA_INTERMEDIATE_PASSWORD}
-    # Store the key to the vault
-    setComponentPassphrase certs "timestamping_${USAGE}_key" \
-                                 "${CERT_KEY}"
+    local TIMESTAMP_CERTIFICAT_TYPE="timestamping"
+    local TIMESTAMP_CERTIFICATE_PATH=$(getTimestampCertificatePath ${TIMESTAMP_CERTIFICAT_TYPE})
+    if [ ! -f "${SERVER_CERTIFICATE_PATH}/${USAGE}.crt" ]; then
+        # Generate the key
+        local CERT_KEY=$(generatePassphrase)
+        # Create the certificate
+        generateTimestampCertificate ${USAGE} \
+                                    ${CERT_KEY} \
+                                    ${CA_INTERMEDIATE_PASSWORD}
+                                    ${TIMESTAMP_CERTIFICAT_TYPE}
+        # Store the key to the vault
+        setComponentPassphrase certs "timestamping_${USAGE}_key" \
+                                    "${CERT_KEY}"
+    else
+        pki_logger "Le certificat ${TIMESTAMP_CERTIFICAT_TYPE} - ${USAGE}.crt existe déjà. Il ne sera pas recrée..."
+    fi
 }
 
 # Génération du certificat client et stockage de la passphrase
@@ -172,27 +209,32 @@ function generateClientCertAndStorePassphrase {
     local COMPONENT="${1}"
     local CLIENT_TYPE="${2}"
 
-    # Récupération du password de la CA_INTERMEDIATE dans le vault-ca
-    CA_INTERMEDIATE_PASSWORD=$(getComponentPassphrase ca "ca_intermediate_${CLIENT_TYPE}")
+    local CLIENT_CERTIFICATE_PATH=$(getClientCertificatePath ${CLIENT_TYPE} ${COMPONENT})
+    if [ ! -d "${CLIENT_CERTIFICATE_PATH}" ]; then
+        # Récupération du password de la CA_INTERMEDIATE dans le vault-ca
+        CA_INTERMEDIATE_PASSWORD=$(getComponentPassphrase ca "ca_intermediate_${CLIENT_TYPE}")
 
-    # Generate the key
-    local CERT_KEY=$(generatePassphrase)
-    # Create the certificate
-    generateClientCertificate ${COMPONENT} \
-                              ${CERT_KEY} \
-                              ${CA_INTERMEDIATE_PASSWORD} \
-                              ${CLIENT_TYPE}
-    # Store the key to the vault
-    setComponentPassphrase certs "client_${CLIENT_TYPE}_${COMPONENT}_key" \
-                                 "${CERT_KEY}"
+        # Generate the key
+        local CERT_KEY=$(generatePassphrase)
+        # Create the certificate
+        generateClientCertificate ${COMPONENT} \
+                                ${CERT_KEY} \
+                                ${CA_INTERMEDIATE_PASSWORD} \
+                                ${CLIENT_TYPE}
+        # Store the key to the vault
+        setComponentPassphrase certs "client_${CLIENT_TYPE}_${COMPONENT}_key" \
+                                    "${CERT_KEY}"
+    else
+        pki_logger "Le certificat CLIENT - ${CLIENT_TYPE} - ${COMPONENT} existe déjà. Il ne sera pas recrée..."
+    fi
 }
 
 # Recopie de la CA de pki/CA vers environments/cert/cert-type/CA
 function copyCAFromPki {
     local CERT_TYPE="${1}"
 
-    pki_logger "Copie de la CA (root + intermediate) de ${CERT_TYPE}"
     mkdir -p "${REPERTOIRE_CERTIFICAT}/${CERT_TYPE}/ca"
+    pki_logger "Copie des CA de ${CERT_TYPE}"
     for CA in $(ls ${REPERTOIRE_CA}/${CERT_TYPE}/*.crt); do
         cp -f "${CA}" "${REPERTOIRE_CERTIFICAT}/${CERT_TYPE}/ca/$(basename ${CA})"
     done
@@ -203,14 +245,19 @@ function copyCAFromPki {
 ######################################################################
 
 cd $(dirname $0)/../..
+ERASE="false"
 
 # Vérification des paramètres
 if [ "${1}" == "" ]; then
     pki_logger "ERROR" "This script needs to know on which environment you want to apply to !"
     exit 1
 fi
+if [ "$#" -gt 1 ]; then
+    if [ "${2,,}" == "true" ]; then
+        ERASE="true"
+    fi
+fi
 ENVIRONNEMENT="${1}"
-
 ENVIRONNEMENT_FILE="${1}"
 
 if [ ! -f "${ENVIRONNEMENT_FILE}" ]; then
@@ -218,61 +265,56 @@ if [ ! -f "${ENVIRONNEMENT_FILE}" ]; then
     exit 1
 fi
 
+pki_logger "Paramètres d'entrée:"
+pki_logger "    -> Environnement: ${ENVIRONNEMENT}"
+pki_logger "    -> Ecraser les certificats existants: ${ERASE}"
+
 # Get consul_domain
 CONSUL_DOMAIN=$(read_ansible_var "consul_domain" "hosts_vitamui_iam_internal[0]")
 
-# Get vitam_site_name
-VITAM_SITE_NAME=$(read_ansible_var "vitam_site_name" "hosts_vitamui_iam_internal[0]")
-
 # Cleaning or creating vault file for certs
-initVault certs
+initVault   certs   ${ERASE}
+
+if [ "${ERASE}" == "true" ]; then
+    if [ -d ${REPERTOIRE_CERTIFICAT} ]; then
+        # We remove all generated certs
+        find "${REPERTOIRE_CERTIFICAT}/" -mindepth 1 -maxdepth 1 -type d -exec rm -Rf {} \;
+    fi
+fi
+if [ ! -d ${REPERTOIRE_CERTIFICAT} ]; then
+    pki_logger "Création du répertoire des certicats sous ${REPERTOIRE_CERTIFICAT}..."
+    mkdir -p ${REPERTOIRE_CERTIFICAT}
+fi
+if [ ! -d ${TEMP_CERTS} ]; then
+    pki_logger "Création du répertoire de travail temporaire tempcerts sous ${TEMP_CERTS}..."
+    mkdir -p ${TEMP_CERTS}
+fi
 
 # Copy CA
 pki_logger "Recopie des clés publiques des CA"
-copyCAFromPki client-iam
-copyCAFromPki client-iam-internal
-# copyCAFromPki client-storage
+copyCAFromPki client-external
+copyCAFromPki client-vitam
 copyCAFromPki server
-# copyCAFromPki timestamping
 
 # Generate hosts certificates
 pki_logger "Génération des certificats serveurs"
-# Method                                    # Component name         # Host group name
-generateHostCertAndStorePassphrase          cas-server          hosts_cas_server
+# Zone interne
 generateHostCertAndStorePassphrase          security-internal   hosts_vitamui_security_internal
 generateHostCertAndStorePassphrase          iam-internal        hosts_vitamui_iam_internal
+#Zone externe
 generateHostCertAndStorePassphrase          iam-external        hosts_vitamui_iam_external
-
+generateHostCertAndStorePassphrase          cas-server          hosts_cas_server
+#Zone UI
 generateHostCertAndStorePassphrase          ui-portal           hosts_ui_portal
 generateHostCertAndStorePassphrase          ui-identity         hosts_ui_identity
 generateHostCertAndStorePassphrase          ui-identity-admin   hosts_ui_identity_admin
+#Reverse
 generateHostCertAndStorePassphrase          reverse             hosts_vitamui_reverseproxy
-# generateHostCertAndStorePassphrase          access-external          hosts_access_external
-# generateHostCertAndStorePassphrase          offer                    hosts_storage_offer_default
-# generateHostCertAndStorePassphrase          ihm-recette              hosts_ihm_recette
-# generateHostCertAndStorePassphrase          ihm-demo                 hosts_ihm_demo
 
-# Generate timestamp certificates
-pki_logger "Génération des certificats timestamping"
-# Method                                    # Usage
-# generateTimestampCertAndStorePassphrase     secure-logbook
-# generateTimestampCertAndStorePassphrase     secure-storage
+# Example of generated client cert for a customer allowing to perform request on external APIs
+generateClientCertAndStorePassphrase        customer_x          client-external
 
-# Generate clients certificates
-pki_logger "Génération des certificats clients"
-# Method                                    # Component name         # Client type
-# generateClientCertAndStorePassphrase        ihm-demo                 client-external
-# generateClientCertAndStorePassphrase        gatling                  client-external
-# generateClientCertAndStorePassphrase        vitam-admin-int          client-external
-# generateClientCertAndStorePassphrase        ihm-recette              client-external
-generateClientCertAndStorePassphrase        reverse                  client-iam
-generateClientCertAndStorePassphrase        iam-external                  client-iam-internal
-# generateClientCertAndStorePassphrase        iam-internal                  client-iam
-# generateClientCertAndStorePassphrase        access_external                  client-external
-
-# Generate storage certificates
-pki_logger "Génération des certificats storage"
-# Method                                    # Component name         # Client type
-# generateClientCertAndStorePassphrase        storage                  client-storage
+# Generate Vitam certificates for VitamUI
+generateClientCertAndStorePassphrase        vitamui             client-vitam
 
 pki_logger "Fin de script"
