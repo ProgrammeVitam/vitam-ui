e4c42a1981ade4607a931adbb8fc9018b2e6f52a
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2020-04-30 15:21:54 +0200

    [TECH] CAS set metrics dependency for prometheus

--- cas/cas-server/run.sh
+++ cas/cas-server/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 mvn clean package -DskipTests
-mkdir -p target/src/main
-rm -rf target/src/main/config
-cp -r src/main/config target/src/main/config
-java -Dspring.config.location=src/main/config/cas-server-application-dev.yml -jar -Xms128m -Xmx512m target/cas-server.war
+java -Dspring.config.additional-location=src/main/config/cas-server-application-dev.yml -jar -Xms128m -Xmx512m target/cas-server.war
