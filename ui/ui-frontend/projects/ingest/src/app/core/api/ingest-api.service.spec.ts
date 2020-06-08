dde0503ce15ebbf3d6afc780217fe8cd25c0d12c
EL HAJJIOUI Nabil <nabil.elhajjioui@smile.fr>
2020-06-08 17:19:22 +0200

    Fix front ingest tests

--- ui/ui-frontend/projects/ingest/src/app/core/api/ingest-api.service.spec.ts
+++ ui/ui-frontend/projects/ingest/src/app/core/api/ingest-api.service.spec.ts
@@ -1,10 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IngestApiService } from './ingest-api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { InjectorModule, LoggerModule, BASE_URL, ENVIRONMENT } from 'ui-frontend-common';
+import { environment } from '../../../environments/environment.prod';
 
 describe('IngestApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      InjectorModule,
+      LoggerModule.forRoot()
+    ],
+    providers: [
+      { provide: BASE_URL, useValue: '/fake-api' },
+      { provide: ENVIRONMENT, useValue: environment }
+    ]
+  }));
   it('should be created', () => {
     const service: IngestApiService = TestBed.get(IngestApiService);
     expect(service).toBeTruthy();
--- ui/ui-frontend/projects/ingest/src/app/ingest/ingest.component.spec.ts
+++ ui/ui-frontend/projects/ingest/src/app/ingest/ingest.component.spec.ts
@@ -1,14 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IngestComponent } from './ingest.component';
+import { MatSidenavModule, MatMenuModule } from '@angular/material';
+import { InjectorModule, LoggerModule } from 'ui-frontend-common';
+import { ActivatedRoute } from '@angular/router';
+import { EMPTY, of } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { VitamUICommonTestModule } from 'ui-frontend-common/testing';
+import { IngestService } from './ingest.service';
 
 describe('IngestComponent', () => {
   let component: IngestComponent;
   let fixture: ComponentFixture<IngestComponent>;
-
+  const ingestServiceMock = {
+    ingest: () => of('test ingest')
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [IngestComponent]
+      imports: [
+        MatMenuModule,
+        MatSidenavModule,
+        InjectorModule,
+        VitamUICommonTestModule,
+        LoggerModule.forRoot()
+      ],
+      declarations: [
+        IngestComponent,
+      ],
+      providers: [
+        { provide: IngestService, useValue: ingestServiceMock },
+        { provide: ActivatedRoute, useValue: { data: EMPTY } },
+        { provide: environment, useValue: environment }
+      ]
     })
       .compileComponents();
   }));
