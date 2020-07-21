f3fd4a869a98ed3a5d2534975ff384eb1a63f7a5
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2020-07-21 18:39:07 +0200

    [TECH] Dev environment : avoid duplication & use certs in dev-deployment in root project

--- ui/ui-portal/run.sh
+++ ui/ui-portal/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mkdir -p target/src/main
-rm -rf target/src/main/config
-cp -r src/main/config target/src/main/config
-
 #retrieve version
 version="$(grep -oP '(?<=>).*?(?=</version>)' pom.xml | grep -v 'version')"
 
-./target/ui-portal-$(echo $version).jar -Xms128m -Xmx512m --spring.config.additional-location=file:src/main/config/ui-portal-application-recette.yml
+java -jar target/ui-portal-$(echo $version).jar -Xms128m -Xmx512m --spring.config.additional-location=file:src/main/config/ui-portal-application-recette.yml
