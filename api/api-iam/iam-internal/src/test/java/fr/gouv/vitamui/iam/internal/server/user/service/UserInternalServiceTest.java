da9e5883731d95e00b0d82d976198a07cf8e47ad
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2020-08-31 00:55:37 +0200

    [TECH] Fix oauth authentication : use same token generation as CAS

--- api/api-iam/iam-internal/src/test/java/fr/gouv/vitamui/iam/internal/server/user/service/UserInternalServiceTest.java
+++ api/api-iam/iam-internal/src/test/java/fr/gouv/vitamui/iam/internal/server/user/service/UserInternalServiceTest.java
@@ -67,7 +67,7 @@ import fr.gouv.vitamui.iam.security.service.InternalSecurityService;
  */
 public final class UserInternalServiceTest {
 
-    private static final String TOKEN_VALUE = "TOK1234567890";
+    private static final String TOKEN_VALUE = "TOK-1-F8lEhVif0FWjgDF32ov73TtKhE6mflRu";
 
     private static final String USER_ID = "userId";
 
