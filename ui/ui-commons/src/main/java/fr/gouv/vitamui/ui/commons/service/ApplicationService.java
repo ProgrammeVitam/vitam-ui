3046ca72c0bcfb2bec1103a2d07632d7b60f983e
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2020-09-08 14:36:36 +0200

    [FIX RABB-808] Fix directory traversal security issue on getLogo

--- ui/ui-commons/src/main/java/fr/gouv/vitamui/ui/commons/service/ApplicationService.java
+++ ui/ui-commons/src/main/java/fr/gouv/vitamui/ui/commons/service/ApplicationService.java
@@ -163,7 +163,7 @@ public class ApplicationService extends AbstractCrudService<ApplicationDto> {
     }
 
     public String getBase64File(String fileName, String basePath) {
-        final Path assetFile = Paths.get(basePath, fileName).normalize();
+        final Path assetFile = Paths.get(basePath, Paths.get(fileName).getFileName().toString());
         String base64Asset = null;
         try {
             base64Asset = DatatypeConverter.printBase64Binary(Files.readAllBytes(assetFile));
