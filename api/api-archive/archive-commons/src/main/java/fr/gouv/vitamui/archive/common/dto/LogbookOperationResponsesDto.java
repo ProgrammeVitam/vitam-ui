9e737d4594f9424d0355f05a9d3822f5ba25809c
ozerouali <oussama.zerouali@smile.fr>
2020-10-09 17:27:24 +0200

    add dto et models

---
+++ api/api-archive/archive-commons/src/main/java/fr/gouv/vitamui/archive/common/dto/LogbookOperationResponsesDto.java
@@ -0,0 +1,8 @@
+package fr.gouv.vitamui.archive.common.dto;
+
+import fr.gouv.vitamui.commons.vitam.api.dto.AbstractVitamUIResponseDto;
+
+public class LogbookOperationResponsesDto extends AbstractVitamUIResponseDto<LogbookOperationModel> {
+
+
+}
