547161b6d811fe43c220b5ce6d801697c0e954f9
ozerouali <oussama.zerouali@smile.fr>
2020-10-09 12:05:17 +0200

    VAS : Add new Archive Module

---
+++ ui/ui-frontend/projects/archive/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ActiveTenantGuard, AppGuard, AuthGuard, AccountComponent } from 'ui-frontend-common';
+import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: AppComponent,
+    canActivate: [AuthGuard, AppGuard],
+    data: { appId: 'PORTAL_APP' }
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [AuthGuard, AppGuard],
+    data: { appId: 'ACCOUNTS_APP' }
+  },
+  {
+    path: 'archive',
+    loadChildren: () => import('./archive/archive.module').then(m => m.ArchiveModule),
+    canActivate: [AuthGuard, AppGuard],
+    data: { appId: 'ARCHIVE_MANAGEMENT_APP' }
+  },
+
+  { path: '**', redirectTo: '' }
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    ActiveTenantGuard,
+    AuthGuard
+  ]
+})
+export class AppRoutingModule { }
