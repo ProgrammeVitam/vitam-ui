6eafb55b4fe5642464b0bc553a2d15f72d2f3d70
Oussama <oussama.zerouali@smile.fr>
2020-10-23 23:12:13 +0200

    add archive frontend

--- ui/ui-frontend/projects/archive/src/app/core/api/archive-api.service.spec.ts
+++ ui/ui-frontend/projects/archive/src/app/core/api/archive-api.service.spec.ts
@@ -1,22 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { ArchiveApiService } from './archive-api.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { InjectorModule, LoggerModule, BASE_URL, ENVIRONMENT } from 'ui-frontend-common';
-import { environment } from '../../../environments/environment.prod';
 
+import { ArchiveApiService } from './archive-api.service';
 
 describe('ArchiveApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule,
-      InjectorModule,
-      LoggerModule.forRoot()
-    ],
-    providers: [
-      { provide: BASE_URL, useValue: '/fake-api' },
-      { provide: ENVIRONMENT, useValue: environment }
-    ]
-  }));
+  beforeEach(() => TestBed.configureTestingModule({}));
+
   it('should be created', () => {
     const service: ArchiveApiService = TestBed.get(ArchiveApiService);
     expect(service).toBeTruthy();
