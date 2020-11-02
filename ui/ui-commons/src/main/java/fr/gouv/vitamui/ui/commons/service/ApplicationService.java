ed8e86cd2801d5459257097ccaf57c99b30425bf
Delphine <delphine.coursier@teamdlab.com>
2020-11-02 17:08:03 +0100

    [TECH] Fix NPE when missing logo conf
    [TECH] Set portal frontend-comon to 1.1.0

--- ui/ui-commons/src/main/java/fr/gouv/vitamui/ui/commons/service/ApplicationService.java
+++ ui/ui-commons/src/main/java/fr/gouv/vitamui/ui/commons/service/ApplicationService.java
@@ -46,6 +46,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -153,6 +154,11 @@ public class ApplicationService extends AbstractCrudService<ApplicationDto> {
     }
 
     public String getBase64File(final String fileName, final String basePath) {
+        if (StringUtils.isBlank(fileName) || StringUtils.isBlank(basePath)) {
+            LOGGER.warn(String.format("Logo information missing : cannot load logo with name \"%s\" in path \"%s\"", fileName, basePath));
+            return null;
+        }
+
         final Path assetFile = Paths.get(basePath, Paths.get(fileName).getFileName().toString());
         String base64Asset = null;
         try {
