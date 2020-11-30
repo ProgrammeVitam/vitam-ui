a2468d1b3308f9aeb8fd3f85d6b7a9de27c09e3f
Delphine <delphine.coursier@teamdlab.com>
2020-11-30 11:24:57 +0100

    [TECH] Fix rebase

--- ui/ui-frontend-common/src/app/modules/components/header/header.module.ts
+++ ui/ui-frontend-common/src/app/modules/components/header/header.module.ts
@@ -5,11 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { HeaderComponent } from './header.component';
 import { ItemSelectModule } from './item-select/item-select.module';
-import { TranslateModule } from '@ngx-translate/core';
 import { MenuModule } from './menu/menu.module';
-import { SelectCustomerModule } from './select-customer/select-customer.module';
+import { SelectLanguageModule } from './select-language/select-language.module';
 import { SelectTenantDialogComponent } from './select-tenant-dialog/select-tenant-dialog.component';
 import { UserPhotoModule } from './user-photo/user-photo.module';
 
@@ -27,7 +27,8 @@ import { UserPhotoModule } from './user-photo/user-photo.module';
     ItemSelectModule,
     UserPhotoModule,
     MenuModule.forRoot(),
-    TranslateModule
+    TranslateModule,
+    SelectLanguageModule
   ],
   exports : [
     HeaderComponent
--- ui/ui-frontend-common/src/app/modules/models/customer/customer.interface.ts
+++ ui/ui-frontend-common/src/app/modules/models/customer/customer.interface.ts
@@ -34,7 +34,6 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
-import {ThemeColors} from '../../utils';
 import { Id } from '../id.interface';
 import { Address } from './address.interface';
 import { OtpState } from './otp-state.enum';
