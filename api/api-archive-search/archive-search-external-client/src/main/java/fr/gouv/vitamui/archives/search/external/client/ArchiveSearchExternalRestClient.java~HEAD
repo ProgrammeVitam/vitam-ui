63ee50ded107be0201ff90ccd7e2a5daf9177920
bbenarbia <benaissa.benarbia@gmail.com>
2020-12-07 13:08:25 +0100

    rename archive  module
    update ent setting and certificats(
    
    add new certs
    
    update mongo scripts
    
    rename module to archive-search and manage certificats
    
    rename ui-archive
    
    generate temp certificats
    
    update certs
    
    generate new certs
    
    rename front end module

--- api/api-archive-search/archive-search-external-client/src/main/java/fr/gouv/vitamui/archives/search/external/client/ArchiveSearchExternalRestClient.java~HEAD
+++ api/api-archive-search/archive-search-external-client/src/main/java/fr/gouv/vitamui/archives/search/external/client/ArchiveSearchExternalRestClient.java~HEAD
@@ -26,6 +26,7 @@
 
 package fr.gouv.vitamui.archives.search.external.client;
 
+import fr.gouv.vitam.common.model.AuditOptions;
 import fr.gouv.vitamui.archives.search.common.dto.ArchiveUnitsDto;
 import fr.gouv.vitamui.archives.search.common.dto.SearchCriteriaDto;
 import fr.gouv.vitamui.archives.search.common.rest.RestApi;
@@ -35,13 +36,16 @@ import fr.gouv.vitamui.commons.api.logger.VitamUILogger;
 import fr.gouv.vitamui.commons.api.logger.VitamUILoggerFactory;
 import fr.gouv.vitamui.commons.rest.client.BasePaginatingAndSortingRestClient;
 import fr.gouv.vitamui.commons.rest.client.ExternalHttpContext;
+import fr.gouv.vitamui.commons.vitam.api.dto.ResultsDto;
 import fr.gouv.vitamui.commons.vitam.api.dto.VitamUISearchResponseDto;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
 import java.util.List;
@@ -116,5 +120,18 @@ public class ArchiveSearchExternalRestClient
         }
         return headers;
     }
+
+    public ResponseEntity<ResultsDto> findUnitById(String id , ExternalHttpContext context) {
+        final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(getUrl() + RestApi.ARCHIVE_UNIT_INFO + CommonConstants.PATH_ID);
+        final HttpEntity<AuditOptions> request = new HttpEntity<>(buildHeaders(context));
+        return restTemplate.exchange(uriBuilder.build(id), HttpMethod.GET, request, ResultsDto.class);
+    }
+
+    public ResponseEntity<Resource> downloadArchiveUnit(String id , ExternalHttpContext context) {
+        final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(getUrl() + RestApi.DOWNLOAD_ARCHIVE_UNIT + CommonConstants.PATH_ID);
+        final HttpEntity<AuditOptions> request = new HttpEntity<>(buildHeaders(context));
+        return restTemplate.exchange(uriBuilder.build(id), HttpMethod.GET, request, Resource.class);
+    }
+
 
 }
--- api/api-archive-search/archive-search-internal/src/main/java/fr/gouv/vitamui/archive/internal/server/service/ArchiveInternalService.java
+++ api/api-archive-search/archive-search-internal/src/main/java/fr/gouv/vitamui/archive/internal/server/service/ArchiveInternalService.java
@@ -26,157 +26,60 @@
 
 package fr.gouv.vitamui.archive.internal.server.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.gouv.vitam.common.client.VitamContext;
 import fr.gouv.vitam.common.database.builder.request.exception.InvalidCreateOperationException;
-import fr.gouv.vitam.common.database.builder.request.single.Select;
 import fr.gouv.vitam.common.exception.InvalidParseOperationException;
 import fr.gouv.vitam.common.exception.VitamClientException;
 import fr.gouv.vitam.common.model.RequestResponse;
-import fr.gouv.vitam.common.model.administration.AgenciesModel;
 import fr.gouv.vitamui.archives.search.common.dsl.VitamQueryHelper;
-import fr.gouv.vitamui.archives.search.common.dto.AgencyResponseDto;
-import fr.gouv.vitamui.archives.search.common.dto.ArchiveUnit;
 import fr.gouv.vitamui.archives.search.common.dto.ArchiveUnitsDto;
 import fr.gouv.vitamui.archives.search.common.dto.SearchCriteriaDto;
-import fr.gouv.vitamui.archives.search.common.dto.SearchCriteriaEltDto;
-import fr.gouv.vitamui.archives.search.common.dto.VitamUIArchiveUnitResponseDto;
-import fr.gouv.vitamui.commons.api.domain.AgencyModelDto;
 import fr.gouv.vitamui.commons.api.domain.DirectionDto;
 import fr.gouv.vitamui.commons.api.exception.BadRequestException;
 import fr.gouv.vitamui.commons.api.logger.VitamUILogger;
 import fr.gouv.vitamui.commons.api.logger.VitamUILoggerFactory;
 import fr.gouv.vitamui.commons.vitam.api.access.UnitService;
-import fr.gouv.vitamui.commons.vitam.api.administration.AgencyService;
-import fr.gouv.vitamui.commons.vitam.api.dto.ResultsDto;
 import fr.gouv.vitamui.commons.vitam.api.dto.VitamUISearchResponseDto;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 /**
- * Archive-Search Internal service communication with VITAM.
+ * Archive Internal service communication with VITAM.
  */
 @Service
 public class ArchiveInternalService {
     private static final VitamUILogger LOGGER =
         VitamUILoggerFactory.getInstance(ArchiveInternalService.class);
     public static final String INGEST_ARCHIVE_TYPE = "INGEST";
-    public static final String ORIGINATING_AGENCY_LABEL_FIELD = "originating_agency_label";
-    public static final String ORIGINATING_AGENCY_ID_FIELD = "#originating_agency";
-    public static final String ARCHIVE_UNIT_DETAILS = "$results";
-    public static final String ARCHIVE_UNIT_USAGE = "qualifier";
-    public static final String ARCHIVE_UNIT_VERSION = "DataObjectVersion";
-
 
     private final ObjectMapper objectMapper;
 
     final private UnitService unitService;
 
-    final private AgencyService agencyService;
+
 
     @Autowired
-    public ArchiveInternalService(final ObjectMapper objectMapper, final UnitService unitService,
-        final AgencyService agencyService) {
+    public ArchiveInternalService(final ObjectMapper objectMapper, final UnitService unitService) {
         this.unitService = unitService;
         this.objectMapper = objectMapper;
-        this.agencyService = agencyService;
-
     }
 
     public ArchiveUnitsDto searchArchiveUnitsByCriteria(final SearchCriteriaDto searchQuery,
         final VitamContext vitamContext)
         throws VitamClientException, IOException {
-        RequestResponse<AgenciesModel> requestResponse =
-            agencyService.findAgencies(vitamContext, new Select().getFinalSelect());
-        final List<AgencyModelDto> actualAgencies = objectMapper
-            .treeToValue(requestResponse.toJsonNode(), AgencyResponseDto.class).getResults();
-
-        mapAgenciesLabelsToAgenciesCodesInCriteria(searchQuery, actualAgencies);
-        Map<String, AgencyModelDto> actualAgenciesMapByIdentifier =
-            actualAgencies.stream().collect(Collectors.toMap(AgencyModelDto::getIdentifier, agency -> agency));
-        LOGGER.debug("calling find archive units by criteria {} ", searchQuery.toString());
+        LOGGER.info("calling find archive units by criteria {} ", searchQuery.toString());
         List<String> archiveUnits = Arrays.asList(INGEST_ARCHIVE_TYPE);
         JsonNode response = searchUnits(mapRequestToDslQuery(archiveUnits, searchQuery), vitamContext);
-        final VitamUISearchResponseDto archivesOriginResponse =
-            objectMapper.treeToValue(response, VitamUISearchResponseDto.class);
-        List<ArchiveUnit> archivesFilled = new ArrayList<>();
-        if (archivesOriginResponse != null) {
-            archivesFilled = archivesOriginResponse.getResults().stream().map(
-                archiveUnit -> fillOriginatingAgencyName(archiveUnit, actualAgenciesMapByIdentifier)
-            ).collect(Collectors.toList());
-        }
-
-        VitamUIArchiveUnitResponseDto responseFilled = new VitamUIArchiveUnitResponseDto();
-        responseFilled.setContext(archivesOriginResponse.getContext());
-        responseFilled.setFacetResults(archivesOriginResponse.getFacetResults());
-        responseFilled.setResults(archivesFilled);
-        responseFilled.setHits(archivesOriginResponse.getHits());
-
-        return new ArchiveUnitsDto(responseFilled);
-    }
-
-    private ArchiveUnit fillOriginatingAgencyName(ResultsDto originResponse,
-        Map<String, AgencyModelDto> actualAgenciesMapById) {
-        ArchiveUnit archiveUnit = new ArchiveUnit();
-        BeanUtils.copyProperties(originResponse, archiveUnit);
-        if (actualAgenciesMapById != null) {
-            AgencyModelDto agencyModel = actualAgenciesMapById.get(originResponse.getOriginatingAgency());
-            if (agencyModel != null) {
-                archiveUnit.setOriginatingAgencyName(agencyModel.getName());
-            }
-        }
-        return archiveUnit;
-    }
-
-    private void mapAgenciesLabelsToAgenciesCodesInCriteria(SearchCriteriaDto searchQuery,
-        List<AgencyModelDto> actualAgencies) {
-        if (searchQuery != null && searchQuery.getCriteriaList() != null && !searchQuery.getCriteriaList().isEmpty()) {
-            List<String> originatingAgencyLabelList = searchQuery.getCriteriaList().stream()
-                .filter(criteria -> ORIGINATING_AGENCY_LABEL_FIELD.equals(criteria.getCriteria()))
-                .map(criteria -> criteria.getValues()).flatMap(List::stream)
-                .collect(Collectors.toList());
-
-            if (originatingAgencyLabelList != null && !originatingAgencyLabelList.isEmpty()) {
-                List<String> filteredAgenciesId = actualAgencies.stream()
-                    .filter(agency -> originatingAgencyLabelList.contains(agency.getName()))
-                    .map(agency -> agency.getIdentifier())
-                    .collect(Collectors.toList());
-                AtomicInteger i = new AtomicInteger();
-                int indexOpt = searchQuery.getCriteriaList().stream().peek(v -> i.incrementAndGet())
-                    .anyMatch(criteria -> ORIGINATING_AGENCY_ID_FIELD.equals(criteria.getCriteria())) ?
-                    i.get() - 1 : -1;
-                SearchCriteriaEltDto agencyCodeCriteria;
-                if (filteredAgenciesId != null && !filteredAgenciesId.isEmpty() && indexOpt != -1) {
-                    agencyCodeCriteria = searchQuery.getCriteriaList().get(indexOpt);
-                    filteredAgenciesId.addAll(agencyCodeCriteria.getValues());
-                    agencyCodeCriteria.setValues(filteredAgenciesId);
-                    searchQuery.getCriteriaList().set(indexOpt, agencyCodeCriteria);
-                } else {
-                    agencyCodeCriteria = new SearchCriteriaEltDto();
-                    agencyCodeCriteria.setCriteria(ORIGINATING_AGENCY_ID_FIELD);
-                    agencyCodeCriteria.setValues(filteredAgenciesId);
-                    searchQuery.getCriteriaList().add(agencyCodeCriteria);
-                }
-                searchQuery.setCriteriaList(searchQuery.getCriteriaList().stream()
-                    .filter(criteria -> !ORIGINATING_AGENCY_LABEL_FIELD.equals(criteria.getCriteria())).collect(
-                        Collectors.toList()));
-            }
-        }
+        return new ArchiveUnitsDto(objectMapper.treeToValue(response, VitamUISearchResponseDto.class));
     }
 
     /**
@@ -221,43 +124,4 @@ public class ArchiveInternalService {
         return response.toJsonNode();
     }
 
-    public ResultsDto findUnitById(String id, VitamContext vitamContext) throws VitamClientException {
-
-        try {
-            LOGGER
-                .info("Archive Unit Infos {}", unitService.findUnitById(id, vitamContext).toJsonNode().get(ARCHIVE_UNIT_DETAILS));
-            String re = StringUtils
-                .chop(unitService.findUnitById(id, vitamContext).toJsonNode().get(ARCHIVE_UNIT_DETAILS).toString().substring(1));
-            return objectMapper.readValue(re, ResultsDto.class);
-        } catch (VitamClientException | JsonProcessingException e) {
-            LOGGER.error("Can not get the archive unit {} ", e);
-            throw new VitamClientException("Unable to find the UA", e);
-        }
-    }
-
-    private String getUsage(String id, VitamContext vitamContext) throws VitamClientException {
-        return unitService.findObjectMetadataById(id, vitamContext).toJsonNode().findValue(ARCHIVE_UNIT_USAGE).textValue();
-    }
-
-    private int getVersion(String id, VitamContext vitamContext) throws VitamClientException {
-        String result = unitService.findObjectMetadataById(id, vitamContext).toJsonNode()
-            .findValue(ARCHIVE_UNIT_VERSION).textValue();
-
-        return Integer.valueOf(result.split("_")[1]);
-
-    }
-
-    public Response downloadArchiveUnit(String id, final VitamContext vitamContext)
-        throws VitamClientException {
-
-        LOGGER.info("Access Contract  {}", vitamContext.getAccessContract());
-        LOGGER.info("Download UA with id {} ", id);
-        try {
-            return unitService
-                .getObjectStreamByUnitId(id, getUsage(id, vitamContext), getVersion(id, vitamContext), vitamContext);
-        } catch (VitamClientException e) {
-            LOGGER.error("Unable to find the UA {} ", e);
-            throw new VitamClientException("Unable to find the UA", e);
-        }
-    }
 }
