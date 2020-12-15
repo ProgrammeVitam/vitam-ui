5133ec64fb09f5ea7784e92d8b40fd4f3e89cd7e
wwang <wwang.works@gmail.com>
2020-12-15 15:14:33 +0100

    [FIX TRTL-559] Review PR

--- ui/ui-frontend/projects/starter-kit/src/app/components/colors/colors.module.ts
+++ ui/ui-frontend/projects/starter-kit/src/app/components/colors/colors.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorsComponent } from './colors.component';
+import { VitamUICommonModule } from 'ui-frontend-common';
 
 
 
 @NgModule({
   declarations: [ColorsComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    VitamUICommonModule],
   exports: [ColorsComponent]
 })
 export class ColorsModule { }
