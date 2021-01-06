715367eb0c4fb9776c1c9a9df3e9341ecacf6954
bbenarbia <benaissa.benarbia@gmail.com>
2021-01-06 19:17:19 +0100

    update ingest labels and truncate title

--- ui/ui-frontend/projects/ingest/src/app/ingest/ingest-list/ingest-list.component.ts
+++ ui/ui-frontend/projects/ingest/src/app/ingest/ingest-list/ingest-list.component.ts
@@ -125,6 +125,7 @@ export class IngestListComponent extends InfiniteScrollTable<any> implements OnD
       criteria['agIdExt.TransferringAgency'] = this._searchText;
       criteria['agIdExt.originatingAgency'] = this._searchText;
       criteria['agIdExt.ArchivalAgreement'] = this._searchText;
+      criteria['evDetData.EvDetailReq'] = this._searchText;
     }
 
     if (this._filters) {
