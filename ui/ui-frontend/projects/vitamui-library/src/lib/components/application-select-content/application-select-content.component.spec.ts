e551cc965e2e7257704fced689d246bf4566f69e
Hamza GRAINI <hamza.graini@xelians.fr>
2021-01-07 20:58:57 +0100

    [FOX-436] Fix linters

--- ui/ui-frontend/projects/vitamui-library/src/lib/components/application-select-content/application-select-content.component.spec.ts
+++ ui/ui-frontend/projects/vitamui-library/src/lib/components/application-select-content/application-select-content.component.spec.ts
@@ -36,8 +36,8 @@
  */
 import {Component, Input} from '@angular/core';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
-
 import {AuthService, WINDOW_LOCATION} from 'ui-frontend-common';
+
 import {UpdatedApplicationSelectContentComponent} from './application-select-content.component';
 
 @Component({
