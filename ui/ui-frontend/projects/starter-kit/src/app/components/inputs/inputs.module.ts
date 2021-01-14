f9ad2274e123dcb578c49b96498c6f39cbd2838b
NOUMANE <ahmed.noumane@xelians.fr>
2021-01-14 17:17:43 +0100

    [US TRTL-644] [REDESIGN] App Archive Research tabs

--- ui/ui-frontend/projects/starter-kit/src/app/components/inputs/inputs.module.ts
+++ ui/ui-frontend/projects/starter-kit/src/app/components/inputs/inputs.module.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { VitamUICommonModule } from 'ui-frontend-common';
 import { InputsComponent } from './inputs.component';
@@ -13,8 +18,13 @@ import { InputsComponent } from './inputs.component';
     VitamUICommonModule,
     ReactiveFormsModule,
     MatSelectModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule
   ],
-  exports: [InputsComponent]
+  exports: [InputsComponent],
+  providers: [{ provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } }],
 })
 export class InputsModule { }
