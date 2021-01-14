974c70eb94db5279d480038de84b4b6cb536a52d
EL HAJJIOUI Nabil <nabil.elhajjioui@smile.fr>
2021-01-14 17:43:41 +0100

    Fix archive upload

--- ui/ui-frontend/projects/ingest/src/app/core/common/upload.service.ts
+++ ui/ui-frontend/projects/ingest/src/app/core/common/upload.service.ts
@@ -152,7 +152,7 @@ export class UploadService {
     }
 
     const formdata: FormData = new FormData();
-    formdata.append('file', file.slice(start, end), file.name);
+    formdata.append('uploadedFile', file.slice(start, end), file.name);
 
     return new HttpRequest('POST', this.ingestApiService.getBaseUrl() + '/ingest/upload', formdata, { headers });
   }
