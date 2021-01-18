77607a55e3f587effceb2c72b3ece283b977a8dc
Ludovic BLANCHET <61453179+Mocteezuma@users.noreply.github.com>
2021-01-18 16:18:40 +0100

    Cherry pick pr107 314 x (#136)
    * Fix archive upload
    
    * add generateDoX method
    
    CA3
    
    download sample docx
    
    add template example
    
    add id to the url
    
    * get the manifest and the ATR as string
    
    add new tables
    
    create the doc file
    
    add generate header method
    
    get values for doc tables
    
    manifest and atr as string
    
    * add titles and generate tables for the report
    
    table 1 generator
    
    table 2 generator
    
    table 3 generator
    
    table 4 generator
    
    delete unused code
    
    * code refactoring and fix conflicts
    
    fix some bugs
    
    add new requested changes
    
    clean code
    
    fix conflicts
    
    * add requested changes
    
    * add logger data
    
    * add new message
    
    * add requested changes
    
    * fix build and run issues
    
    correct services
    
    fix ingest-preview issue
    
    fix ingest front test
    
    fix somme issues
    
    * fix ansible delete
    
    Co-authored-by: EL HAJJIOUI Nabil <nabil.elhajjioui@smile.fr>
    Co-authored-by: Oussama <oussama.zerouali@smile.fr>

--- api/api-ingest/ingest-external/src/main/java/fr/gouv/vitamui/ingest/external/server/rest/IngestExternalController.java
+++ api/api-ingest/ingest-external/src/main/java/fr/gouv/vitamui/ingest/external/server/rest/IngestExternalController.java
@@ -49,7 +49,6 @@ import fr.gouv.vitamui.ingest.common.rest.RestApi;
 import fr.gouv.vitamui.ingest.external.server.service.IngestExternalService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -114,5 +113,12 @@ public class IngestExternalController {
         }
 
         return ingestExternalService.upload(in, action, contextId);
+    }
+
+    @Secured(ServicesData.ROLE_GET_INGEST)
+    @GetMapping(RestApi.INGEST_REPORT_DOCX + CommonConstants.PATH_ID)
+    public ResponseEntity<byte[]> generateDocX(final @PathVariable("id") String id) {
+        LOGGER.debug("export docx report for ingest with id :{}", id);
+        return ingestExternalService.generateDocX(id);
     }
 }
