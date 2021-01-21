7d303990039178fac40ad030cf07b713e8bae808
Cindy <cindy.nacibide@teamdlab.com>
2021-01-21 12:13:32 +0100

    [US TRTL-682] use list of group form for translations

--- ui/ui-frontend/projects/identity/src/app/customer/customer-preview/homepage-message-tab/homepage-message/homepage-message.component.ts
+++ ui/ui-frontend/projects/identity/src/app/customer/customer-preview/homepage-message-tab/homepage-message/homepage-message.component.ts
@@ -10,6 +10,7 @@ import {
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Customer, Theme, ThemeService } from 'ui-frontend-common';
 
 @Component({
@@ -38,36 +39,18 @@ export class HomepageMessageComponent implements OnInit, OnDestroy {
 
   private defaultTheme: Theme = this.themeService.defaultTheme;
 
-  private messageTranslations: {
-    id: number;
-    language: string;
-    title: string;
-    description: string;
-  }[] = [];
-
-  private validTranslations: {
-    id: number;
-    isValid: boolean;
-  }[] = [];
-
   constructor(
     public dialogRef: MatDialogRef<HomepageMessageComponent>,
     private formBuilder: FormBuilder,
     private themeService: ThemeService
   ) { }
 
-  ngOnDestroy(): void {
-    this.destroy.next();
-  }
-
   ngOnInit() {
-    this.homepageMessageForm = this.formBuilder.group({
-      id: null,
-      portalTitle: ['', [Validators.required]],
-      portalMessage: ['', [Validators.required, Validators.maxLength(500)]],
-      messageTranslations: [null],
-      isFormValid: null,
-    });
+
+    let idCustomer = null;
+    if (this.customer && this.customer.id) {
+      idCustomer = this.customer.id;
+    }
 
     this.portalMessage =
       this.customer && this.customer.portalMessage
@@ -78,84 +61,43 @@ export class HomepageMessageComponent implements OnInit, OnDestroy {
         ? this.customer.portalTitle
         : this.defaultTheme.portalTitle;
 
-    this.homepageMessageForm.get('portalTitle').setValue(this.portalTitle);
-    this.homepageMessageForm.get('portalMessage').setValue(this.portalMessage);
-    this.homepageMessageForm.get('messageTranslations').setValue(this.messageTranslations);
-    this.homepageMessageForm.get('isFormValid').setValue(true);
-
-    if (this.customer && this.customer.id) {
-      this.homepageMessageForm.get('id').setValue(this.customer.id);
-    }
+    this.homepageMessageForm = this.formBuilder.group({
+      id: idCustomer,
+      portalTitle: [this.portalTitle, [Validators.required]],
+      portalMessage: [this.portalMessage, [Validators.required, Validators.maxLength(500)]],
+      translations: this.formBuilder.array([])
+    });
 
     this.formToSend.emit({ form: this.homepageMessageForm });
 
-    this.homepageMessageForm.valueChanges.subscribe(() => {
+    this.homepageMessageForm.valueChanges.pipe(takeUntil(this.destroy)).subscribe(() => {
       this.formToSend.emit({ form: this.homepageMessageForm });
     });
   }
 
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
+  }
+
   public onAdd(): void {
-    this.messageTranslations = this.homepageMessageForm.get('messageTranslations').value;
+    const emptyTranslation = this.formBuilder.group({
+      language: ['', Validators.required],
+      portalTitle: ['', [Validators.required]],
+      portalMessage: ['', [Validators.required, Validators.maxLength(500)]],
+  });
 
-    let newId = 1;
-    const itemsLength = this.messageTranslations?.length;
-
-    if (itemsLength > 0) {
-      newId = this.messageTranslations[itemsLength - 1].id + 1;
-    }
-
-    const emptyTranslation = {
-      id: newId,
-      language: '',
-      title: '',
-      description: ''
-    };
-
-    this.messageTranslations.push(emptyTranslation);
+    this.homepageMessageForm.get('translations').value.push(emptyTranslation);
+    this.formToSend.emit({ form: this.homepageMessageForm });
   }
 
-  public updateTranslation(data: { form: FormGroup }): void {
-    const idValue = data.form.get('id')?.value;
-
-    this.messageTranslations = [...this.homepageMessageForm.get('messageTranslations').value];
-
-    this.messageTranslations.filter(f => f.id === idValue).map(t => {
-      t.language = data.form.get('language')?.value;
-      t.title = data.form.get('portalTitle')?.value;
-      t.description = data.form.get('portalMessage')?.value;
-      return t;
-    });
-
-    const validationForm = {
-      id: idValue,
-      isValid: data.form.valid
-    };
-
-    const i = this.validTranslations.findIndex((item: { id: any; }) => item.id === idValue);
-    if (i > -1) { this.validTranslations[i] = validationForm; } else { this.validTranslations.push(validationForm); }
-
-    this.checkValidation();
-
-    this.homepageMessageForm.get('messageTranslations').patchValue(this.messageTranslations);
+  public update(): void {
+    this.formToSend.emit({ form: this.homepageMessageForm });
   }
 
-  public removeTranslation(data: { form: FormGroup }): void {
-    const idValue = data.form.get('id')?.value;
+  public remove(index: number): void {
 
-    this.messageTranslations = [...this.homepageMessageForm.get('messageTranslations').value];
-
-    this.messageTranslations = this.messageTranslations.filter(f => f.id !== idValue);
-    this.validTranslations = [];
-    this.validTranslations = this.validTranslations.filter(f => f.id !== idValue);
-
-    this.homepageMessageForm.get('messageTranslations').patchValue(this.messageTranslations);
-  }
-
-  private checkValidation() {
-    let valid = true;
-    if (this.validTranslations.some((v) => !v.isValid)) {
-      valid = false;
-    }
-    this.homepageMessageForm.get('isFormValid').patchValue(valid);
+    this.homepageMessageForm.get('translations').value.splice(index, 1);
+    this.formToSend.emit({ form: this.homepageMessageForm });
   }
 }
