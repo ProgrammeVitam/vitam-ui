f58e9c52629e89f65af50feb3dc0d53a823c19ea
pybelecalo <pierre-yves.belecalo@xelians.fr>
2021-01-22 18:52:31 +0100

    [TECH] Fix import after merge

--- ui/ui-frontend/projects/ingest/src/app/ingest/ingest-list/ingest-list.module.ts
+++ ui/ui-frontend/projects/ingest/src/app/ingest/ingest-list/ingest-list.module.ts
@@ -36,10 +36,10 @@
  */
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatProgressSpinnerModule } from '@angular/material';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { VitamUICommonModule } from 'ui-frontend-common';
 import { IngestListComponent } from './ingest-list.component';
-import { VitamUICommonModule } from "ui-frontend-common";
 
 
 @NgModule({
