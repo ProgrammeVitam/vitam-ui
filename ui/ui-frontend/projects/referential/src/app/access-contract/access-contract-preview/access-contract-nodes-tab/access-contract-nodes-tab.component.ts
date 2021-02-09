ee7efe5a6731c16206b05cf0e6a15ec0b088e6c6
Maël QUAISSARD <mael.quaissard@smile.fr>
2021-02-09 15:28:22 +0100

    VITAM-UI-090: Fix getByDSL api calling

--- ui/ui-frontend/projects/referential/src/app/access-contract/access-contract-preview/access-contract-nodes-tab/access-contract-nodes-tab.component.ts
+++ ui/ui-frontend/projects/referential/src/app/access-contract/access-contract-preview/access-contract-nodes-tab/access-contract-nodes-tab.component.ts
@@ -67,7 +67,7 @@ export class AccessContractNodesTabComponent implements OnInit {
     this.searchAccessContractId = event.value;
     let headers = new HttpHeaders().append('Content-Type', 'application/json');
     headers = headers.append('X-Access-Contract-Id', event.value);
-    this.unitService.getByDsl(this.getDslForRootNodes(), headers).subscribe(
+    this.unitService.getByDsl(null, this.getDslForRootNodes(), headers).subscribe(
       response => {
         if (response.httpCode === 200) {
           this.titles = {};
