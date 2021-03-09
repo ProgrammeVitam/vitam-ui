613d6f0b4944990fe2166f7e86497ab65462106f
Maël QUAISSARD <mael.quaissard@smile.fr>
2021-03-09 18:24:40 +0100

    TULEAP-20654: adapt the audit download according to the event types

--- ui/ui-frontend/projects/referential/src/app/audit/audit.service.ts
+++ ui/ui-frontend/projects/referential/src/app/audit/audit.service.ts
@@ -34,12 +34,12 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {AccessionRegister, Event} from 'projects/vitamui-library/src/public-api';
 import {Observable} from 'rxjs';
 import {map, tap} from 'rxjs/operators';
-import {DEFAULT_PAGE_SIZE, Direction, PageRequest, SearchService} from 'ui-frontend-common';
+import {DEFAULT_PAGE_SIZE, Direction, PageRequest, SearchService, LogbookApiService} from 'ui-frontend-common';
 
 import {AccessionRegisterApiService} from '../core/api/accession-register-api.service';
 import {OperationApiService} from '../core/api/operation-api.service';
@@ -52,6 +52,7 @@ export class AuditService extends SearchService<Event> {
 
   constructor(
     private operationApiService: OperationApiService,
+    private logbookApiService: LogbookApiService,
     private accessionRegisterApiService: AccessionRegisterApiService,
     private snackBar: VitamUISnackBar,
     http: HttpClient) {
@@ -90,18 +91,49 @@ export class AuditService extends SearchService<Event> {
       );
   }
 
-  download(id: string, accessContractId: string) {
-    this.operationApiService.downloadOperation(
-      id, 'AUDIT', new HttpHeaders({'X-Access-Contract-Id': accessContractId})).subscribe((blob) => {
-        const element = document.createElement('a');
-        element.href = window.URL.createObjectURL(blob);
-        element.download = id + '.json';
-        element.style.visibility = 'hidden';
-        document.body.appendChild(element);
-        element.click();
-        document.body.removeChild(element);
-      });
+  download(id: string, eventType: string, accessContractId: string) {
+    const headers:HttpHeaders = new HttpHeaders({'X-Access-Contract-Id': accessContractId});
+    let downloadObservable: Observable<HttpResponse<Blob> | Blob>;
+    let fileName: string;
+    let downloadType: string;
 
+    if (eventType === 'EXPORT_PROBATIVE_VALUE' || eventType === 'RECTIFICATION_AUDIT') {
+      fileName = id + '.json';
+      downloadType = 'report';
+      downloadObservable = this.logbookApiService.downloadReport(id, downloadType, headers);
+    } else if (eventType === 'EVIDENCE_AUDIT' || eventType === 'PROCESS_AUDIT') {
+      fileName = id + '.jsonl';
+      downloadType = 'batchreport';
+      downloadObservable = this.logbookApiService.downloadReport(id, downloadType, headers)
+    } else {
+      fileName = id + '.json';
+      downloadType = 'AUDIT';
+      downloadObservable = this.operationApiService.downloadOperation(id, downloadType, headers);
+    }
+
+    downloadObservable.subscribe(response => {
+      const element = document.createElement('a');
+
+      let blob: Blob;
+      if (response instanceof HttpResponse) {
+        blob = new Blob([response.body], {type: 'octet/stream'})
+      } else {
+        blob = response;
+      }
+      const url = window.URL.createObjectURL(blob);
+      element.href = url;
+
+      element.download = fileName;
+      element.style.visibility = 'hidden';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    }, error => {
+      this.snackBar.open(error.error.message, null, {
+        panelClass: 'vitamui-snack-bar',
+        duration: 10000
+      });
+    });
   }
 
   getAllAccessionRegister(accessContractId: string): Observable<AccessionRegister[]> {
