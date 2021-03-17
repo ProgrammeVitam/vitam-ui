c56ace8d23d81d5464b9a1035c04a12f7aade969
Maël QUAISSARD <mael.quaissard@smile.fr>
2021-03-17 12:30:58 +0100

    [CEA] Multiple fix Referential Apps lot2
    * TULEAP-24039: Add the parameterized access contract on the probative value creation screen
    * TULEAP-24039: Fix unit tests
    * TULEAP-24040: Add the parameterized access contract on the operation preview screen
    * TULEAP-24039: Add the parameterized access contract on the audit creation screen
    * Fix icon
    * Add missing icons
    * TULEAP-20350 : Disable agency name modification
    * TULEAP-20178: Handle activation and deactivation ingest contract dates
    * Fix activation and deactivation dates on access contract creation
    * Fix font id

--- ui/ui-frontend/projects/referential/src/app/probative-value/probative-value-create/probative-value-create.component.spec.ts
+++ ui/ui-frontend/projects/referential/src/app/probative-value/probative-value-create/probative-value-create.component.spec.ts
@@ -6,28 +6,30 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatSelectModule} from '@angular/material/select';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {EMPTY, of} from 'rxjs';
-import {ConfirmDialogService} from 'ui-frontend-common';
+import {ConfirmDialogService, ExternalParametersService} from 'ui-frontend-common';
 import {VitamUICommonTestModule} from 'ui-frontend-common/testing';
 
-import {AccessContractService} from '../../access-contract/access-contract.service';
 import {ProbativeValueService} from '../probative-value.service';
 import {ProbativeValueCreateComponent} from './probative-value-create.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('ProbativeValueCreateComponent', () => {
   let component: ProbativeValueCreateComponent;
   let fixture: ComponentFixture<ProbativeValueCreateComponent>;
 
   beforeEach(waitForAsync(() => {
-
-    const accessContractServiceMock = {
-      getAll: () => of([])
+    const parameters: Map<string, string> = new Map<string, string>();
+    const externalParametersServiceMock = {
+      getUserExternalParameters: () => of(parameters)
     };
+
     TestBed.configureTestingModule({
       imports: [
         ReactiveFormsModule,
         MatSelectModule,
         NoopAnimationsModule,
         MatProgressBarModule,
+        MatSnackBarModule,
         VitamUICommonTestModule
       ],
       declarations: [ProbativeValueCreateComponent],
@@ -37,7 +39,7 @@ describe('ProbativeValueCreateComponent', () => {
         {provide: MAT_DIALOG_DATA, useValue: {}},
         {provide: ConfirmDialogService, useValue: {listenToEscapeKeyPress: () => EMPTY}},
         {provide: ProbativeValueService, useValue: {}},
-        {provide: AccessContractService, useValue: accessContractServiceMock}
+        {provide: ExternalParametersService, useValue: externalParametersServiceMock}
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
