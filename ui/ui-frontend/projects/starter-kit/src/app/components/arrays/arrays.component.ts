792db2b25346f90368bdb1bff20d863af0adcf97
Delphine <delphine.coursier@teamdlab.com>
2021-03-31 12:42:28 +0200

    [TECH] Improve starter-kit

--- ui/ui-frontend/projects/starter-kit/src/app/components/arrays/arrays.component.ts
+++ ui/ui-frontend/projects/starter-kit/src/app/components/arrays/arrays.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { collapseAnimation, Direction, Group, rotateAnimation } from 'ui-frontend-common';
@@ -8,30 +7,31 @@ import { SampleDialogComponent } from '../miscellaneous/sample-dialog/sample-dia
   selector: 'app-arrays',
   templateUrl: './arrays.component.html',
   styleUrls: ['./arrays.component.scss'],
-  animations: [
-    collapseAnimation,
-    rotateAnimation,
-  ]
+  animations: [collapseAnimation, rotateAnimation],
 })
 export class ArraysComponent implements OnInit {
-
   public orderBy = 'name';
   public direction = Direction.ASCENDANT;
-  public levelFilterOptions: Array<{ value: string, label: string }> = [];
+  public levelFilterOptions: Array<{ value: string; label: string }> = [];
   public filterMap: { [key: string]: any[] } = { status: ['ENABLED'], level: null };
 
   public dataSource = [
-    {name: 'Sample name', identifier: '0001', description: 'Sample description', level: 'Hero'},
-    {name: 'Sample name', identifier: '0002', description: 'Sample description', level: 'Hero'},
-    {name: 'Sample name', identifier: '0003', description: 'Sample description', level: 'Hero'},
-    {name: 'Sample name', identifier: '0004', description: 'Sample description', level: 'Hero'},
-    {name: 'Sample name', identifier: '0005', description: 'Sample description', level: 'Hero'},
-    {name: 'Sample name', identifier: '0006', description: 'Sample description', level: 'Hero'},
+    {
+      name: 'Sample name',
+      identifier: '0001',
+      description: 'Sample description with a very long text that will trigger ellipsis with tooltip',
+      level: 'Hero',
+    },
+    { name: 'Sample name', identifier: '0002', description: 'Sample description', level: 'Hero' },
+    { name: 'Sample name', identifier: '0003', description: 'Sample description', level: 'Hero' },
+    { name: 'Sample name', identifier: '0004', description: 'Sample description', level: 'Hero' },
+    { name: 'Sample name', identifier: '0005', description: 'Sample description', level: 'Hero' },
+    { name: 'Sample name', identifier: '0006', description: 'Sample description', level: 'Hero' },
   ] as Group[];
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   public onFilterChange(key: string, values: any[]): void {
     this.filterMap[key] = values;
@@ -43,9 +43,11 @@ export class ArraysComponent implements OnInit {
 
   openDialog(event: any) {
     console.log('[Dialog] : ' + event);
-    this.dialog.open(SampleDialogComponent, { panelClass: 'vitamui-modal', disableClose: true }).afterClosed().subscribe(() => {
-      console.log('Dialog closed !');
-    });
+    this.dialog
+      .open(SampleDialogComponent, { panelClass: 'vitamui-modal', disableClose: true })
+      .afterClosed()
+      .subscribe(() => {
+        console.log('Dialog closed !');
+      });
   }
-
 }
