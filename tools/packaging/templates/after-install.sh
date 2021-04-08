4f66a0be369d5b26f48942be2cfd08ff9d0a9255
Gordolo <romain.gordolo@xelians.fr>
2021-04-08 11:35:38 +0200

    [vas] Debian packaging

--- tools/packaging/templates/after-install.sh
+++ tools/packaging/templates/after-install.sh
@@ -1,5 +1,5 @@
 # After first install
-if [ $1 -eq 1 ] ; then
+#if [ $1 -eq 1 ] ; then
         # Ensure rights for vitamui service file system
         chown __USER__:__GROUP__  -R   /vitamui/*/__NAME__/
         chown vitamui:vitamui /vitamui/ /vitamui/*
@@ -7,5 +7,5 @@ if [ $1 -eq 1 ] ; then
         chmod 0750 /vitamui/*/*
 
         # Initial installation
-        systemctl preset __USER__-__NAME__.service >/dev/null 2>&1 || :
-fi
+        systemctl preset __USER__-__NAME__.service #>/dev/null 2>&1 || :
+#fi
