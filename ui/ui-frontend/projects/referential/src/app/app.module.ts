027cb2c4822885522f064c4f0b1491b06988e1a3
oussamasic <oussamazerouali42@gmail.com>
2021-07-09 11:22:03 +0200

    fixe referential applications issue

--- ui/ui-frontend/projects/referential/src/app/app.module.ts
+++ ui/ui-frontend/projects/referential/src/app/app.module.ts
@@ -1,35 +1,33 @@
-import {registerLocaleData} from '@angular/common';
-import localeFr from '@angular/common/locales/fr';
-import {LOCALE_ID, NgModule} from '@angular/core';
-import {MatNativeDateModule} from '@angular/material/core';
-import {BrowserModule, Title} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgxFilesizeModule} from 'ngx-filesize';
-import {MultiTranslateHttpLoader} from 'ngx-translate-multi-http-loader';
-import {VitamUICommonModule, WINDOW_LOCATION} from 'ui-frontend-common';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {CoreModule} from './core/core.module';
-import {SharedModule} from './shared/shared.module';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {HttpClient} from '@angular/common/http';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
+import { registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { default as localeFr } from '@angular/common/locales/fr';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { NgxFilesizeModule } from 'ngx-filesize';
+import { QuicklinkModule } from 'ngx-quicklink';
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
+import { VitamUICommonModule, VitamuiMissingTranslationHandler, WINDOW_LOCATION } from 'ui-frontend-common';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 export function httpLoaderFactory(httpClient: HttpClient): MultiTranslateHttpLoader {
-  return new MultiTranslateHttpLoader(httpClient,[
-    {prefix: './assets/shared-i18n/',suffix: '.json'},
-    {prefix: './assets/i18n/',suffix: '.json'}
+  return new MultiTranslateHttpLoader(httpClient, [
+    { prefix: './assets/shared-i18n/', suffix: '.json' },
+    { prefix: './assets/i18n/', suffix: '.json' },
   ]);
 }
 
 registerLocaleData(localeFr, 'fr');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     CoreModule,
     BrowserAnimationsModule,
@@ -39,22 +37,19 @@ registerLocaleData(localeFr, 'fr');
     MatNativeDateModule,
     NgxFilesizeModule,
     SharedModule,
+    QuicklinkModule,
     TranslateModule.forRoot({
+      missingTranslationHandler: { provide: MissingTranslationHandler, useClass: VitamuiMissingTranslationHandler },
       defaultLanguage: 'fr',
       loader: {
         provide: TranslateLoader,
         useFactory: httpLoaderFactory,
-        deps: [HttpClient]
-      }
+        deps: [HttpClient],
+      },
     }),
-    ServiceWorkerModule.register('ngsw-worker.js',{enabled: environment.production}),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
-  providers: [
-    Title,
-    {provide: LOCALE_ID, useValue: 'fr'},
-    {provide: WINDOW_LOCATION, useValue: window.location}
-  ],
+  providers: [Title, { provide: LOCALE_ID, useValue: 'fr' }, { provide: WINDOW_LOCATION, useValue: window.location }],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
