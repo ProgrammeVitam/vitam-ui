d58cef3a849901b72b15603432036da3b7b46fe0
Julien CORNILLE <julien.cornille@xelians.fr>
2021-07-19 17:58:47 +0200

    [TRV-201] Add primary properties

---
+++ commons/commons-api/src/test/java/fr/gouv/vitamui/commons/api/instance/InstanceServiceTest.java
@@ -0,0 +1,41 @@
+package fr.gouv.vitamui.commons.api.instance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.env.Environment;
+
+@ExtendWith(MockitoExtension.class)
+class InstanceServiceTest {
+
+    @Mock
+    private Environment environment;
+
+    @InjectMocks
+    private InstanceService service;
+
+    @Test
+    void shouldBeTrue_whenPropertiesIsTrue() {
+        given(environment.getProperty(anyString(), any(Class.class))).willReturn(true);
+        assertThat(service.isPrimary()).isTrue();
+    }
+
+    @Test
+    void shouldBeTrue_whenPropertiesIsMissing() {
+        given(environment.getProperty(anyString(), any(Class.class))).willReturn(null);
+        assertThat(service.isPrimary()).isTrue();
+    }
+
+    @Test
+    void shouldBeTrue_whenPropertiesIsFalse() {
+        given(environment.getProperty(anyString(), any(Class.class))).willReturn(false);
+        assertThat(service.isPrimary()).isFalse();
+    }
+
+}
