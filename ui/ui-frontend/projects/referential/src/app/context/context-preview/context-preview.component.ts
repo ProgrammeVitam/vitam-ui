1457d8605f5ad3211494aeb2d37b32222dd318e7
Benaissa BENARBIA <ben.benarbia@gmail.com>
2021-09-08 10:44:33 +0200

    Revert "TULEAP 24198: Synchronize permission tab"
    This reverts commit e02adc8c96f7e16bd149bce2ef2226161891a709.

--- ui/ui-frontend/projects/referential/src/app/context/context-preview/context-preview.component.ts
+++ ui/ui-frontend/projects/referential/src/app/context/context-preview/context-preview.component.ts
@@ -86,6 +86,7 @@ export class ContextPreviewComponent implements AfterViewInit {
   async checkBeforeExit() {
     if (await this.confirmAction()) {
       const submitAccessContractUpdate: Observable<Context> = this.tabLinks[this.tabs.selectedIndex].prepareSubmit();
+
       submitAccessContractUpdate.subscribe(() => {
         this.contextService.get(this.context.identifier).subscribe(
           response => {
@@ -93,17 +94,14 @@ export class ContextPreviewComponent implements AfterViewInit {
           }
         );
       });
+    } else {
+      this.tabLinks[this.tabs.selectedIndex].resetForm(this.context);
     }
   }
 
   async interceptTabChange(tab: MatTab, tabHeader: MatTabHeader, idx: number) {
     if (this.tabUpdated[this.tabs.selectedIndex]) {
       await this.checkBeforeExit();
-    } else {
-      if (idx === 1) {
-        // Reset the permissions for if the context has changed
-        this.tabLinks[idx].resetForm(this.context);
-      }
     }
 
     const args = [tab, tabHeader, idx];
