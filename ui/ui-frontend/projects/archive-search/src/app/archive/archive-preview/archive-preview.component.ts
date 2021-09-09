a6aefc5059f036568fddd80605baf845eeb7c80d
Passam KASSEM <passam.kassem@focati-solutions.com>
2021-09-09 16:43:16 +0200

    [VAS] BUG 7428 : APP Recherche, le champ 'chemin' sur le panneau d'information d'une UA devrait reconstituer le chemin de l'UA

--- ui/ui-frontend/projects/archive-search/src/app/archive/archive-preview/archive-preview.component.ts
+++ ui/ui-frontend/projects/archive-search/src/app/archive/archive-preview/archive-preview.component.ts
@@ -1,58 +1,61 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
- import { Unit } from '../models/unit.interface';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Unit } from '../models/unit.interface';
 import { HttpHeaders } from '@angular/common/http';
 import { ArchiveService } from '../archive.service';
-import { StartupService } from 'ui-frontend-common';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-archive-preview',
   templateUrl: './archive-preview.component.html',
-  styleUrls: ['./archive-preview.component.scss']
+  styleUrls: ['./archive-preview.component.scss'],
 })
-export class ArchivePreviewComponent  implements OnInit {
-
+export class ArchivePreviewComponent implements OnInit, OnChanges {
   @Input()
   archiveUnit: Unit;
   @Input()
-  accessContract : string;
+  accessContract: string;
   @Output()
   previewClose: EventEmitter<any> = new EventEmitter();
   @Input()
   isPopup: boolean;
 
-
   tenantIdentifier: string;
-  constructor(private archiveService : ArchiveService, private startupService : StartupService, private route: ActivatedRoute) {
-    this.route.params.subscribe(params => {
+  uaPath$: Observable<{ fullPath: string; resumePath: string }>;
+  fullPath = false;
+
+  constructor(private archiveService: ArchiveService, private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
       this.tenantIdentifier = params.tenantIdentifier;
     });
-
   }
 
   ngOnInit() {
+    this.uaPath$ = this.archiveService.buildArchiveUnitPath(this.archiveUnit, this.accessContract);
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['archiveUnit'].firstChange) {
+      return;
     }
-
+    if (changes['archiveUnit'].currentValue['#id'] !== changes['archiveUnit'].previousValue['#id']) {
+      this.uaPath$ = this.archiveService.buildArchiveUnitPath(this.archiveUnit, this.accessContract);
+    }
+    this.fullPath = false;
+  }
 
   onDownloadObjectFromUnit(archiveUnit: Unit) {
     let headers = new HttpHeaders().append('Content-Type', 'application/json');
     headers = headers.append('X-Access-Contract-Id', this.accessContract);
 
-    return this.archiveService.downloadObjectFromUnit(archiveUnit['#id'], archiveUnit?.Title , archiveUnit?.Title_, headers);
+    return this.archiveService.downloadObjectFromUnit(archiveUnit['#id'], archiveUnit?.Title, archiveUnit?.Title_, headers);
   }
 
   emitClose() {
     this.previewClose.emit();
   }
 
-
-
-  openPopup() {
-    window.open(this.startupService.getConfigStringValue('UI_URL')
-      + '/archive-search/tenant/' + this.tenantIdentifier + '/' + this.accessContract + '/id/' + this.archiveUnit['#id'],
-        'detailPopup', 'width=684, height=713, resizable=no, location=no');
-    this.emitClose();
+  showArchiveUniteFullPath() {
+    this.fullPath = true;
   }
-
 }
