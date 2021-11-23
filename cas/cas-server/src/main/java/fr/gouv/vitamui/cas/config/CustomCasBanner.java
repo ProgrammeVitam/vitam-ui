d3db103c0887dfce3657f30d805ecf1a7384ce48
Jérôme LELEU <jerome.leleu@teamdlab.com>
2021-11-23 17:10:43 +0100

    WIP running

---
+++ cas/cas-server/src/main/java/fr/gouv/vitamui/cas/config/CustomCasBanner.java
@@ -0,0 +1,21 @@
+package fr.gouv.vitamui.cas.config;
+
+import org.apereo.cas.util.spring.boot.AbstractCasBanner;
+
+/**
+ * The Vitam UI custom abnner.
+ */
+public class CustomCasBanner extends AbstractCasBanner {
+
+    @Override
+    protected String getTitle() {
+        return "   _______           _____  __      ___ _                  _    _ _______  \n" +
+            "  / / ____|   /\\    / ____| \\ \\    / (_) |                | |  | |_   _\\ \\ \n" +
+            " | | |       /  \\  | (___    \\ \\  / / _| |_ __ _ _ __ ___ | |  | | | |  | |\n" +
+            " | | |      / /\\ \\  \\___ \\    \\ \\/ / | | __/ _` | '_ ` _ \\| |  | | | |  | |\n" +
+            " | | |____ / ____ \\ ____) |    \\  /  | | || (_| | | | | | | |__| |_| |_ | |\n" +
+            " | |\\_____/_/    \\_\\_____/      \\/   |_|\\__\\__,_|_| |_| |_|\\____/|_____|| |\n" +
+            "  \\_\\                                                                  /_/ \n" +
+            "                                                                           \n";
+    }
+}
