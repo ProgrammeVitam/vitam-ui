53b343b576adc947f0d6caf2352b8346ff61b7d0
Abdelmoez GUETAT <abdelmoez.guetat@smile.fr>
2022-05-19 16:36:12 +0200

    [VAS] Story #9919 - add collect-external and collect-internal

---
+++ api/api-collect/collect-external/src/main/java/fr/gouv/vitamui/collect/external/server/service/CollectExternalService.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2022)
+ *
+ * contact.vitam@culture.gouv.fr
+ *
+ * This software is a computer program whose purpose is to implement a digital archiving back-office system managing
+ * high volumetry securely and efficiently.
+ *
+ * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
+ * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
+ * circulated by CEA, CNRS and INRIA at the following URL "https://cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
+ * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
+ * successive licensors have only limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
+ * developing or reproducing the software by the user in light of its specific status of free software, that may mean
+ * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
+ * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
+ * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
+ * accept its terms.
+ */
+
+package fr.gouv.vitamui.collect.external.server.service;
+
+
+import fr.gouv.vitamui.collect.common.dto.ProjectDto;
+import fr.gouv.vitamui.collect.internal.client.CollectInternalRestClient;
+import fr.gouv.vitamui.collect.internal.client.CollectInternalWebClient;
+import fr.gouv.vitamui.commons.api.ParameterChecker;
+import fr.gouv.vitamui.commons.api.domain.DirectionDto;
+import fr.gouv.vitamui.commons.api.domain.PaginatedValuesDto;
+import fr.gouv.vitamui.iam.security.client.AbstractResourceClientService;
+import fr.gouv.vitamui.iam.security.service.ExternalSecurityService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+/**
+ * The service to create project.
+ */
+@Getter
+@Setter
+@Service
+public class CollectExternalService extends AbstractResourceClientService<ProjectDto, ProjectDto> {
+
+    private final CollectInternalRestClient collectInternalRestClient;
+
+    private final CollectInternalWebClient projectInternalWebClient;
+
+    @Autowired
+    public CollectExternalService(CollectInternalRestClient collectInternalRestClient,
+        CollectInternalWebClient projectInternalWebClient, final ExternalSecurityService externalSecurityService) {
+        super(externalSecurityService);
+        this.collectInternalRestClient = collectInternalRestClient;
+        this.projectInternalWebClient = projectInternalWebClient;
+    }
+
+    @Override
+    protected CollectInternalRestClient getClient() {
+        return collectInternalRestClient;
+    }
+
+    @Override
+    public PaginatedValuesDto<ProjectDto> getAllPaginated(final Integer page, final Integer size,
+        final Optional<String> criteria, final Optional<String> orderBy, final Optional<DirectionDto> direction) {
+        ParameterChecker.checkPagination(size, page);
+        return getClient().getAllPaginated(getInternalHttpContext(), page, size, criteria, orderBy, direction);
+    }
+}
