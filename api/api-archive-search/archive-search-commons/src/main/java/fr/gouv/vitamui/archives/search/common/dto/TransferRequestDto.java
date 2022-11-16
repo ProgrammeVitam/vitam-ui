ca01fd04a00145f18c93979f93983a8a28d75bb6
Benaissa BENARBIA <ben.benarbia@gmail.com>
2022-11-16 19:14:17 +0100

    bug/vas-10545: handle threshold for DIP export, Mgt rules update, elimination and transfer

--- api/api-archive-search/archive-search-commons/src/main/java/fr/gouv/vitamui/archives/search/common/dto/TransferRequestDto.java
+++ api/api-archive-search/archive-search-commons/src/main/java/fr/gouv/vitamui/archives/search/common/dto/TransferRequestDto.java
@@ -48,5 +48,4 @@ public class TransferRequestDto implements Serializable {
     private SearchCriteriaDto searchCriteria;
     private Set<String> dataObjectVersions;
     private boolean lifeCycleLogs;
-    private Long maxSizeThreshold;
 }
