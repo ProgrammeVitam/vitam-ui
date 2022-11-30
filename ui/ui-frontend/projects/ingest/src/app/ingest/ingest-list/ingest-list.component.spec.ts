58a77744e7847a16cc7830658033fbc4576f89e5
Oussama ZEROUALI <oussamazerouali42@gmail.com>
2022-11-30 17:13:46 +0100

    [VAS] activate ingest Angular Tests (#1120)

--- ui/ui-frontend/projects/ingest/src/app/ingest/ingest-list/ingest-list.component.spec.ts
+++ ui/ui-frontend/projects/ingest/src/app/ingest/ingest-list/ingest-list.component.spec.ts
@@ -1,10 +1,37 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+/*
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2015-2022)
+ *
+ * contact.vitam@culture.gouv.fr
+ *
+ * This software is a computer program whose purpose is to implement a digital archiving back-office system managing
+ * high volumetry securely and efficiently.
+ *
+ * This software is governed by the CeCILL 2.1 license under French law and abiding by the rules of distribution of free
+ * software. You can use, modify and/ or redistribute the software under the terms of the CeCILL 2.1 license as
+ * circulated by CEA, CNRS and INRIA at the following URL "https://cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy, modify and redistribute granted by the license,
+ * users are provided only with a limited warranty and the software's author, the holder of the economic rights, and the
+ * successive licensors have only limited liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated with loading, using, modifying and/or
+ * developing or reproducing the software by the user in light of its specific status of free software, that may mean
+ * that it is complicated to manipulate, and that also therefore means that it is reserved for developers and
+ * experienced professionals having in-depth computer knowledge. Users are therefore encouraged to load and test the
+ * software's suitability as regards their requirements in conditions enabling the security of their systems and/or data
+ * to be ensured and, more generally, to use and operate it in the same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
+ * accept its terms.
+ */
 
-import { IngestListComponent } from './ingest-list.component';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { IngestService } from '../ingest.service';
-import { of } from 'rxjs';
 import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { IngestService } from '../ingest.service';
+import { IngestListComponent } from './ingest-list.component';
 
 describe('IngestListComponent', () => {
   let component: IngestListComponent;
@@ -12,17 +39,16 @@ describe('IngestListComponent', () => {
 
   const ingestServiceMock = {
     search: () => of([]),
-    getAllPaginated: () => of([])
+    getAllPaginated: () => of([]),
   };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ IngestListComponent ],
-      imports : [TranslateModule.forRoot()],
+      declarations: [IngestListComponent],
+      imports: [TranslateModule.forRoot()],
       providers: [{ provide: IngestService, useValue: ingestServiceMock }],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -31,7 +57,7 @@ describe('IngestListComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('component should be created', () => {
     expect(component).toBeTruthy();
   });
 });
