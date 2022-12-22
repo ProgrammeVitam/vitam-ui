3809905d8224ff5bb403521ce429beecaf8d53fa
Benaissa <ben.benarbia@gmail.com>
2022-12-22 19:47:29 +0100

    update hash

--- deployment/download_repo.sh
+++ deployment/download_repo.sh
@@ -2,6 +2,6 @@
 set -e
 
 BASEDIR=$(dirname "$0")
-wget --user bbenarbia --password 'StyleBleu$1ok' --recursive --directory-prefix=$BASEDIR/repo/ --no-host-directories --cut-dirs=1 --no-parent -R "index.html*" -l3 -k "http://repository.dev.programmevitam.fr/contrib/b070acd87e1ce9c472847499d7648ebf76de8eba/deb/"
+wget --user bbenarbia --password 'StyleBleu$1ok' --recursive --directory-prefix=$BASEDIR/repo/ --no-host-directories --cut-dirs=1 --no-parent -R "index.html*" -l3 -k "http://repository.dev.programmevitam.fr/contrib/c4e8b600a6a48f4cba5faa2571afb0f78909619f/deb/"
 
 #launch it whith :  start_repo.sh "commit_id"
