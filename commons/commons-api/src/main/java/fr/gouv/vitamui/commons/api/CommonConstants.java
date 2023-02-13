665e18a6e40afca8e4f268ea4ee2443a72af0aac
Daniel Radeau <daniel.radeau.ext@culture.gouv.fr>
2023-02-13 17:09:52 +0100

    Bug #10888 - fix: internal server error in file formats viewer
    changes:
    
    * fix bad api hydration in frontend component common option tab
    * fix bad usage of endsWith method in history ui backend controller
    * remove puid encoding in external referential api history call
    * add tests on get file-formats and history logbooks

--- commons/commons-api/src/main/java/fr/gouv/vitamui/commons/api/CommonConstants.java
+++ commons/commons-api/src/main/java/fr/gouv/vitamui/commons/api/CommonConstants.java
@@ -353,4 +353,5 @@ public class CommonConstants {
     public static final String X_SIZE_TOTAL = "X-Size-Total";
     public static final String X_CHUNK_OFFSET = "X-Chunk-Offset";
     public static final String LOGO = "LOGO";
+    public static final String HISTORY = "history";
 }
