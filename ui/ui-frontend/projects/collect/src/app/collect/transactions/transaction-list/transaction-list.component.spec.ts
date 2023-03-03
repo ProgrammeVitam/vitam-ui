f9a641ca2888372367f0e151ae16cbd8ed06c2c9
Benaissa BENARBIA <ben.benarbia@gmail.com>
2023-03-04 00:22:46 +0100

    fix collect unit test (#1247)

--- ui/ui-frontend/projects/collect/src/app/collect/transactions/transaction-list/transaction-list.component.spec.ts
+++ ui/ui-frontend/projects/collect/src/app/collect/transactions/transaction-list/transaction-list.component.spec.ts
@@ -44,7 +44,7 @@ import {
   StartupService,
   Transaction,
   TransactionStatus,
-  WINDOW_LOCATION,
+  WINDOW_LOCATION
 } from 'ui-frontend-common';
 import { environment } from '../../../../../../archive-search/src/environments/environment';
 import { VitamUISnackBar } from '../../shared/vitamui-snack-bar';
@@ -75,6 +75,7 @@ describe('TransactionListComponent', () => {
     submissionAgencyIdentifier: 'submissionAgencyIdentifier',
     archiveProfile: 'archivalProfile',
     comment: 'comment',
+    legalStatus:'A legal status'
   };
 
   const matDialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['open']);
