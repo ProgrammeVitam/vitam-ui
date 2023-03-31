6e8e1f81494f70a2560ab2222b33029ee6e4dfe0
Paris Alexandre <ldr.paris@gmail.com>
2023-03-31 18:12:10 +0200

    [VAS] Bug 11305: add some comments

--- commons/commons-api/src/main/java/fr/gouv/vitamui/commons/api/domain/AccessionRegisterSearchDto.java
+++ commons/commons-api/src/main/java/fr/gouv/vitamui/commons/api/domain/AccessionRegisterSearchDto.java
@@ -26,7 +26,6 @@ public class AccessionRegisterSearchDto {
     private String elimination;
     private String transferReply;
 
-
     @Data
     public static class EndDateInterval {
         private String endDateMin;
