50fcc6562f287e46e2cb7770f88f3c90911e52d7
Alexandre <ldr.paris@gmail.com>
2023-04-20 15:40:00 +0200

    [VAS] Bug 11340: clean audit collect toggle (#1311)

--- ui/ui-frontend/projects/vitamui-library/src/lib/components/vitamui-radio/vitamui-radio.component.ts
+++ ui/ui-frontend/projects/vitamui-library/src/lib/components/vitamui-radio/vitamui-radio.component.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:component-selector */
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { VitamUIRadioGroupService } from '../vitamui-radio-group/vitamui-radio-group.service';
 
@@ -10,19 +10,20 @@ import { VitamUIRadioGroupService } from '../vitamui-radio-group/vitamui-radio-g
 })
 export class VitamUIRadioComponent implements OnInit {
 
-  @Input() value: string;
+  @Input()
+  value: string;
 
-  @Input() label: string;
+  @Input()
+  label: string;
 
-  @Input() checked: boolean;
-
-  @HostBinding('class.primary') @Input() primary = false;
+  @Input()
+  checked: boolean;
 
   constructor(private radioGroupService: VitamUIRadioGroupService) {
   }
 
   ngOnInit() {
-    this.checked = (this.checked !== undefined);
+    this.checked = this.checked !== undefined;
   }
 
   changed() {
