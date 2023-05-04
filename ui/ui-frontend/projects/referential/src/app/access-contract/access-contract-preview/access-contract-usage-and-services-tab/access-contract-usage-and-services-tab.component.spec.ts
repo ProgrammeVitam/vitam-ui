ef9b5e91a0e34cc3daa7f81e23d95e8bbf15250e
Daniel RADEAU <d.radeau@gmail.com>
2023-05-04 16:08:54 +0200

    story #11387 fix(referential): fix bad behavior in usage and service tab of access contracts (#1328)
    Co-authored-by: Daniel Radeau <daniel.radeau.ext@culture.gouv.fr>

--- ui/ui-frontend/projects/referential/src/app/access-contract/access-contract-preview/access-contract-usage-and-services-tab/access-contract-usage-and-services-tab.component.spec.ts
+++ ui/ui-frontend/projects/referential/src/app/access-contract/access-contract-preview/access-contract-usage-and-services-tab/access-contract-usage-and-services-tab.component.spec.ts
@@ -39,7 +39,6 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { of } from 'rxjs';
-import { AccessContract } from 'ui-frontend-common';
 import { VitamUICommonTestModule } from 'ui-frontend-common/testing';
 import { AgencyService } from '../../../agency/agency.service';
 import { AccessContractService } from '../../access-contract.service';
@@ -49,54 +48,14 @@ describe('AccessContractUsageAndServicesTabComponent', () => {
   let component: AccessContractUsageAndServicesTabComponent;
   let fixture: ComponentFixture<AccessContractUsageAndServicesTabComponent>;
 
-  const accessContractValue = {
-    everyOriginatingAgency: true,
-    originatingAgencies: ['test'],
-    everyDataObjectVersion: true,
-    dataObjectVersion: ['test'],
-  };
-
-  const previousValue: AccessContract = {
-    tenant: 0,
-    version: 1,
-    description: 'desc',
-    status: 'ACTIVE',
-    id: 'vitam_id',
-    name: 'Name',
-    identifier: 'SP-000001',
-    everyOriginatingAgency: true,
-    originatingAgencies: ['test'],
-    everyDataObjectVersion: true,
-    dataObjectVersion: ['test'],
-    creationDate: '01-01-20',
-    lastUpdate: '01-01-20',
-    activationDate: '01-01-20',
-    deactivationDate: '01-01-20',
-    writingPermission: true,
-    writingRestrictedDesc: true,
-    accessLog: '',
-    ruleFilter: true,
-    ruleCategoryToFilter: ['rule'],
-    rootUnits: [],
-    excludedRootUnits: [],
-  };
-
-  const agencyServiceMock = {
-    getAll: () => of([]),
-  };
-  const accessContractServiceMock = {
-    // tslint:disable-next-line:variable-name
-    patch: (_data: any) => of(null),
-  };
-
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, VitamUICommonTestModule],
       declarations: [AccessContractUsageAndServicesTabComponent],
       providers: [
         FormBuilder,
-        { provide: AccessContractService, useValue: accessContractServiceMock },
-        { provide: AgencyService, useValue: agencyServiceMock },
+        { provide: AccessContractService, useValue: { patch: (_data: any) => of(null) } },
+        { provide: AgencyService, useValue: { getAll: () => of([]) } },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
@@ -105,8 +64,12 @@ describe('AccessContractUsageAndServicesTabComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AccessContractUsageAndServicesTabComponent);
     component = fixture.componentInstance;
-    component.form.setValue(accessContractValue);
-    component.previousValue = (): AccessContract => previousValue;
+    component.form.setValue({
+      everyOriginatingAgency: true,
+      originatingAgencies: ['test'],
+      everyDataObjectVersion: true,
+      dataObjectVersion: ['test'],
+    });
     fixture.detectChanges();
   });
 
