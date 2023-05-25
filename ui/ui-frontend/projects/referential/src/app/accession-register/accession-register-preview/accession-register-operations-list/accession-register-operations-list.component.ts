93fe1449fb9a3328281ab1ea0d58018f9e623c13
Benaissa BENARBIA <ben.benarbia@gmail.com>
2023-05-25 15:15:01 +0200

    fix error

--- ui/ui-frontend/projects/referential/src/app/accession-register/accession-register-preview/accession-register-operations-list/accession-register-operations-list.component.ts
+++ ui/ui-frontend/projects/referential/src/app/accession-register/accession-register-preview/accession-register-operations-list/accession-register-operations-list.component.ts
@@ -24,10 +24,9 @@
  * The fact that you are presently reading this means that you have had knowledge of the CeCILL 2.1 license and that you
  * accept its terms.
  */
-import { animate, AUTO_STYLE, state, style, transition, trigger } from '@angular/animations';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { TranslateService} from '@ngx-translate/core';
+import {animate, AUTO_STYLE, state, style, transition, trigger} from '@angular/animations';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 import {Direction, RegisterValueEventModel, RegisterValueEventType} from 'ui-frontend-common';
 
 @Component({
