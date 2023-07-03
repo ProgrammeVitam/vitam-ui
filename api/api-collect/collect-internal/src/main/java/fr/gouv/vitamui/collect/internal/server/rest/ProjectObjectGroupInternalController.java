8c75eb6d9b6e1bcb477494b7dda0bd6a58cb4c5e
Paris Alexandre <ldr.paris@gmail.com>
2023-07-03 11:30:24 +0200

    [VAS] Bug 11487: download blocked because context was lost

--- api/api-collect/collect-internal/src/main/java/fr/gouv/vitamui/collect/internal/server/rest/ProjectObjectGroupInternalController.java
+++ api/api-collect/collect-internal/src/main/java/fr/gouv/vitamui/collect/internal/server/rest/ProjectObjectGroupInternalController.java
@@ -29,6 +29,7 @@
 
 package fr.gouv.vitamui.collect.internal.server.rest;
 
+import fr.gouv.vitam.common.client.VitamContext;
 import fr.gouv.vitam.common.exception.InvalidParseOperationException;
 import fr.gouv.vitam.common.exception.VitamClientException;
 import fr.gouv.vitamui.collect.common.rest.RestApi;
@@ -70,7 +71,7 @@ public class ProjectObjectGroupInternalController {
         VitamUILoggerFactory.getInstance(ProjectObjectGroupInternalController.class);
     private final ProjectObjectGroupInternalService projectObjectGroupInternalService;
     private final ExternalParametersService externalParametersService;
-    private static final String IDENTIFIER_MANDATORY = "The identifier is a mandatory parameter: ";
+    private static final String IDENTIFIER_MANDATORY = "The identifier is mandatory parameter: ";
 
     public ProjectObjectGroupInternalController(ProjectObjectGroupInternalService projectObjectGroupInternalService,
         final ExternalParametersService externalParametersService) {
@@ -87,12 +88,10 @@ public class ProjectObjectGroupInternalController {
         ParameterChecker
             .checkParameter(IDENTIFIER_MANDATORY, id);
         SanityChecker.checkSecureParameter(id, usage);
+        VitamContext context = externalParametersService.buildVitamContextFromExternalParam();
         LOGGER.debug("Download Archive Unit Object with id {}", id);
-
         return Mono.<Resource>fromCallable(() -> {
-                Response response =
-                    projectObjectGroupInternalService.downloadObjectFromUnit(id, usage, version,
-                        externalParametersService.buildVitamContextFromExternalParam());
+                Response response = projectObjectGroupInternalService.downloadObjectFromUnit(id, usage, version, context);
                 return new InputStreamResource((InputStream) response.getEntity());
             }).subscribeOn(Schedulers.boundedElastic())
             .flatMap(resource -> Mono.just(ResponseEntity
