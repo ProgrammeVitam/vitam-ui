f557b3ba2bbc74c5d211bab1717f8952355527a0
oussamasic <oussamazerouali42@gmail.com>
2023-12-12 16:48:56 +0100

    call collect upload objects API from getorix Frontend

--- ui/ui-frontend/projects/collect/src/app/getorix-deposit/getorix-deposit-upload-object/getorix-deposit-upload-object.component.ts
+++ ui/ui-frontend/projects/collect/src/app/getorix-deposit/getorix-deposit-upload-object/getorix-deposit-upload-object.component.ts
@@ -25,11 +25,14 @@
  * accept its terms.
  */
 
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { BreadCrumbData, Logger } from 'ui-frontend-common';
+import { CollectUploadFile, CollectZippedUploadFile } from '../../collect/shared/collect-upload/collect-upload-file';
+import { CollectUploadService } from '../../collect/shared/collect-upload/collect-upload.service';
 import { GetorixDeposit } from '../core/model/getorix-deposit.interface';
 import { GetorixDepositService } from '../getorix-deposit.service';
 
@@ -43,16 +46,33 @@ export class GetorixDepositUploadObjectComponent implements OnInit, OnDestroy {
   operationId: string;
   dataBreadcrumb: BreadCrumbData[];
   getorixDepositDetails: GetorixDeposit;
+  myList: string[] = ['test1', 'test2', 'test3', 'hello', 'world'];
+  isIndeterminate: boolean;
+  isAllChecked: boolean;
+
+  // upload
+  uploadFiles$: Observable<CollectUploadFile[]>;
+  zippedFile$: Observable<CollectZippedUploadFile>;
+  @ViewChild('fileSearch', { static: false }) fileSearch: any;
+  hasDropZoneOver = false;
+  hasError = false;
 
   constructor(
     private route: ActivatedRoute,
     private getorixDepositService: GetorixDepositService,
     private router: Router,
     private translateService: TranslateService,
-    private loggerService: Logger
+    private loggerService: Logger,
+    private uploadService: CollectUploadService,
+    private snackBar: MatSnackBar,
+    private translationService: TranslateService,
+    private logger: Logger
   ) {}
 
   ngOnInit(): void {
+    this.uploadFiles$ = this.uploadService.getUploadingFiles();
+    this.zippedFile$ = this.uploadService.getZipFile();
+
     this.operationIdentifierSubscription = this.route.params.subscribe((params) => {
       this.operationId = params.operationIdentifier;
       this.dataBreadcrumb = [
@@ -69,6 +89,8 @@ export class GetorixDepositUploadObjectComponent implements OnInit, OnDestroy {
       this.getorixDepositService.getGetorixDepositById(params.operationIdentifier).subscribe(
         (data: GetorixDeposit) => {
           this.getorixDepositDetails = data;
+          // search units of the transactionId
+          // create unit-list component
         },
         (error) => {
           this.loggerService.error('error while searching for this operation', error);
@@ -89,5 +111,128 @@ export class GetorixDepositUploadObjectComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.operationIdentifierSubscription?.unsubscribe();
+  }
+
+  addNewObject() {
+    console.log('add new object');
+  }
+
+  editUnits() {
+    console.log('edit units');
+  }
+
+  deleteUnits() {
+    console.log('delete units');
+  }
+
+  reclassifyUnits() {
+    console.log('reclassify units');
+  }
+
+  notifyArchivist() {
+    console.log('send notification to the archivist');
+  }
+
+  checkParentBoxChange(event: any) {
+    if (event.target.checked) {
+      this.isAllChecked = true;
+    } else {
+      this.isAllChecked = false;
+    }
+  }
+
+  checkChildrenBoxChange(id: string, event: any) {
+    console.log('element', id);
+    const action = event.target.checked;
+
+    if (this.isAllChecked && !action) {
+      this.isIndeterminate = true;
+    } else {
+      if (action) {
+        this.isIndeterminate = false;
+      }
+    }
+  }
+
+  onDragOver(event: any) {
+    event.preventDefault();
+    this.hasDropZoneOver = true;
+  }
+
+  onDragLeave(event: any) {
+    event.preventDefault();
+    this.hasDropZoneOver = false;
+  }
+
+  async onDropped(event: any) {
+    this.hasDropZoneOver = false;
+    event.preventDefault();
+    const items = event.dataTransfer.items;
+    const exists = this.uploadService.directoryExistInZipFile(items, true);
+    if (exists) {
+      this.snackBar.open(this.translationService.instant('GETORIX_DEPOSIT.UPLOAD_ARCHIVES.UPLOAD_FILE_ALREADY_IMPORTED'), null, {
+        duration: 3000,
+      });
+      return;
+    }
+    await this.uploadService.handleDragAndDropUpload(items).then(() => {
+      return this.sendFilesToVitam();
+    });
+  }
+
+  async handleFile(event: any) {
+    event.preventDefault();
+    const items = event.target.files;
+    const exists = this.uploadService.directoryExistInZipFile(items, false);
+    if (exists) {
+      this.snackBar.open(this.translationService.instant('GETORIX_DEPOSIT.UPLOAD_ARCHIVES.UPLOAD_FILE_ALREADY_IMPORTED'), null, {
+        duration: 3000,
+      });
+      return;
+    }
+    await this.uploadService
+      .handleUpload(items)
+
+      .then(() => {
+        return this.sendFilesToVitam();
+      });
+  }
+
+  addFolder() {
+    this.fileSearch.nativeElement.click();
+  }
+
+  removeFolder(file: CollectUploadFile) {
+    this.uploadService.removeFolder(file);
+  }
+
+  deleteAllFiles() {
+    this.uploadFiles$.subscribe((data) => {
+      data.forEach((file) => {
+        this.removeFolder(file);
+      });
+    });
+  }
+
+  async sendFilesToVitam() {
+    return this.uploadService
+      .uploadZip(this.getorixDepositDetails.tenantIdentifier, this.getorixDepositDetails.transactionId)
+      .then((uploadOperation) => {
+        uploadOperation.subscribe(
+          () => {},
+          (error: any) => {
+            this.logger.error('Error while uploading files to Vitam', error);
+          },
+          () => {
+            this.snackBar.open(this.translationService.instant('GETORIX_DEPOSIT.UPLOAD_ARCHIVES.TERMINATED'), null, {
+              panelClass: 'vitamui-snack-bar',
+              duration: 10000,
+            });
+          }
+        );
+      })
+      .catch((error) => {
+        this.logger.error('Error while uploading files to Vitam', error);
+      });
   }
 }
