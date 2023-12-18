7d6fd4f7d52c2675caa7998dde482d7e3d3e7a67
Romain GORDOLO <romain.gordolo@xelians.fr>
2023-12-18 18:02:14 +0100

    fix identity-admin

--- tools/packaging/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-for rpm_file in `find . -name "*.rpm"`
-do
-    rpm_name=$(basename $rpm_file)
-    curl -v --netrc-file /etc/curl.netrc --upload-file $rpm_file https://nexus.teamdlab.com/repository/yum-xelians/$1/$rpm_name
-done
-
