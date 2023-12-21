4617baf0e579dc790006f69f9a025c646f414134
Pierre-Yves Bele Calo <pyXelians@vi.com>
2023-12-21 10:37:41 +0100

    Fix import rules

--- api/api-referential/referential-external/src/main/java/fr/gouv/vitamui/referential/external/server/rest/RuleExternalController.java
+++ api/api-referential/referential-external/src/main/java/fr/gouv/vitamui/referential/external/server/rest/RuleExternalController.java
@@ -36,36 +36,21 @@
  */
 package fr.gouv.vitamui.referential.external.server.rest;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Optional;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import fr.gouv.vitam.common.exception.InvalidParseOperationException;
 import fr.gouv.vitamui.common.security.SafeFileChecker;
 import fr.gouv.vitamui.common.security.SanityChecker;
 import fr.gouv.vitamui.commons.api.CommonConstants;
 import fr.gouv.vitamui.commons.api.ParameterChecker;
-import fr.gouv.vitamui.referential.common.dto.RuleDto;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import fr.gouv.vitamui.commons.api.domain.DirectionDto;
 import fr.gouv.vitamui.commons.api.domain.PaginatedValuesDto;
 import fr.gouv.vitamui.commons.api.domain.ServicesData;
+import fr.gouv.vitamui.commons.api.exception.BadRequestException;
 import fr.gouv.vitamui.commons.api.exception.PreconditionFailedException;
 import fr.gouv.vitamui.commons.api.logger.VitamUILogger;
 import fr.gouv.vitamui.commons.api.logger.VitamUILoggerFactory;
 import fr.gouv.vitamui.commons.rest.util.RestUtils;
 import fr.gouv.vitamui.commons.vitam.api.dto.LogbookOperationsResponseDto;
-import fr.gouv.vitamui.referential.common.dto.RuleDto;
 import fr.gouv.vitamui.referential.common.dto.RuleDto;
 import fr.gouv.vitamui.referential.common.rest.RestApi;
 import fr.gouv.vitamui.referential.external.server.service.RuleExternalService;
@@ -79,25 +64,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -219,20 +185,18 @@ public class RuleExternalController {
 
     /***
      * Import agencies from a csv file
-     * @param fileName the file name
      * @param file the agency csv file to import
      * @return the vitam response
      */
     @Secured(ServicesData.ROLE_IMPORT_RULES)
     @PostMapping(CommonConstants.PATH_IMPORT)
-    public JsonNode importRules(@RequestParam("fileName") String fileName, @RequestParam("file") MultipartFile file) {
-        if(file != null) {
-            SafeFileChecker.checkSafeFilePath(file.getOriginalFilename());
-            SanityChecker.isValidFileName(file.getOriginalFilename());
+    public JsonNode importRules(@RequestParam("file") MultipartFile file) {
+        LOGGER.debug("Import referential file {}", file != null ? file.getOriginalFilename() : null);
+        if (file == null) {
+            throw new BadRequestException("No file to check .");
         }
-        SanityChecker.isValidFileName(fileName);
-        SafeFileChecker.checkSafeFilePath(fileName);
-        LOGGER.debug("Import agency file {}", fileName);
-        return ruleExternalService.importRules(fileName, file);
+        SafeFileChecker.checkSafeFilePath(file.getOriginalFilename());
+        SanityChecker.isValidFileName(file.getOriginalFilename());
+        return ruleExternalService.importRules(file.getOriginalFilename(), file);
     }
 }
