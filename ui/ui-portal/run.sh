57739e958cc780e73fab9fadba5b5464fe09c734
Benaissa BENARBIA <benaissa.benarbia.ext@culture.gouv.fr>
2024-04-11 19:42:07 +0200

    Story#12674: delete ui componenets

--- ui/ui-portal/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#retrieve version
-version="$(grep -oP '(?<=>).*?(?=</version>)' pom.xml | grep -v 'version')"
-
-java -jar target/ui-portal-$(echo $version).jar -Xms128m -Xmx512m --spring.profiles.active=dev,recette
