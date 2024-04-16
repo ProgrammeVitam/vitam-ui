3a592dcc51b0e62d5fea80b22ed744ca70a53185
Benaissa BENARBIA <ben.benarbia@gmail.com>
2024-04-16 14:02:43 +0200

    Story #12363: Update VitamUI dependencies (#1755)
    * Story#12363: update Vitamui dependecies
    
    ---------
    
    Co-authored-by: Benaissa BENARBIA <benaissa.benarbia.ext@culture.gouv.fr>

--- cas/cas-server/src/test/java/fr/gouv/vitamui/cas/webflow/actions/GeneralTerminateSessionActionTest.java
+++ cas/cas-server/src/test/java/fr/gouv/vitamui/cas/webflow/actions/GeneralTerminateSessionActionTest.java
@@ -1,10 +1,17 @@
 package fr.gouv.vitamui.cas.webflow.actions;
 
+import fr.gouv.vitamui.cas.BaseWebflowActionTest;
+import fr.gouv.vitamui.commons.api.identity.ServerIdentityAutoConfiguration;
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.apereo.cas.logout.LogoutManager;
 import org.apereo.cas.services.RegexRegisteredService;
 import org.apereo.cas.services.ServicesManager;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 
@@ -16,7 +23,10 @@ import static org.mockito.Mockito.when;
  *
  *
  */
-public final class GeneralTerminateSessionActionTest {
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ServerIdentityAutoConfiguration.class)
+@TestPropertySource(locations = "classpath:/application-test.properties")
+public final class GeneralTerminateSessionActionTest extends BaseWebflowActionTest {
 
     private static final String LOGOUT_URL = "http://dev.vitamui.com:8080/cas/app1/callback";
 
