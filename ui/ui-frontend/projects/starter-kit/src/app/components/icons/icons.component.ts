cd9354898f0c80209e323ca1d7f1d43a1a88072e
Maxime Robert <maxime.robert@smile.fr>
2024-04-22 18:51:49 +0200

    Fix #12513 recharting regression: re-add missing icons
    + automatically list every icon in starter kit

--- ui/ui-frontend/projects/starter-kit/src/app/components/icons/icons.component.ts
+++ ui/ui-frontend/projects/starter-kit/src/app/components/icons/icons.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,10 +6,43 @@ import { FormControl } from '@angular/forms';
   templateUrl: './icons.component.html',
   styleUrls: ['./icons.component.scss'],
 })
-export class IconsComponent implements OnInit {
-  public control = new FormControl();
+export class IconsComponent {
+  icons: (string | string[])[];
+  coloredIcons: { [key: string]: string[] };
+  colors = ['primary', 'secondary', 'danger', 'success', 'warning', 'light'];
+  selectedColor: string = '';
 
-  constructor() {}
+  constructor() {
+    const cssRules = Array.from(document.styleSheets)
+      .reduce((acc, v) => {
+        try {
+          return acc.concat(Array.from(v.cssRules));
+        } catch (e) {
+          return acc;
+        }
+      }, [])
+      .filter((css) => css.cssText?.startsWith('.vitamui-icon-'))
+      .map((css) => css.cssText.split(':')[0].split('vitamui-icon-')[1])
+      .sort((a, b) => a.localeCompare(b));
 
-  ngOnInit() {}
+    const obj = cssRules.reduce((acc, cssRule) => {
+      const [icon, path] = cssRule.split(' .');
+      acc[icon] = acc[icon] || [];
+      if (path) {
+        acc[icon] = [...new Set([...acc[icon], path])];
+      }
+      return acc;
+    }, {});
+
+    this.icons = Object.keys(obj);
+    this.coloredIcons = Object.entries(obj)
+      .filter(([_, value]) => value)
+      .reduce(
+        (acc, [key, value]) => {
+          acc[key] = value as string[];
+          return acc;
+        },
+        {} as { [key: string]: string[] },
+      );
+  }
 }
--- ui/ui-frontend/projects/starter-kit/src/app/components/icons/icons.module.ts
+++ ui/ui-frontend/projects/starter-kit/src/app/components/icons/icons.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 import { VitamUICommonModule } from 'ui-frontend-common';
 import { IconsComponent } from './icons.component';
 
 @NgModule({
   declarations: [IconsComponent],
-  imports: [CommonModule, VitamUICommonModule],
+  imports: [CommonModule, VitamUICommonModule, FormsModule, MatOptionModule, MatSelectModule],
   exports: [IconsComponent],
 })
 export class IconsModule {}
