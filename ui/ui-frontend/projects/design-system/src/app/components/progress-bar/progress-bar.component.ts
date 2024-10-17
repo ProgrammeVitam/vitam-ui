5338a26b8268f01750299a7925f3a752f2a04435
Maxime Robert <maxime.robert@smile.fr>
2024-10-17 17:19:08 +0200

    Fix #13552: fix table and progress bar

--- ui/ui-frontend/projects/design-system/src/app/components/progress-bar/progress-bar.component.ts
+++ ui/ui-frontend/projects/design-system/src/app/components/progress-bar/progress-bar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonProgressBarModule } from 'vitamui-library';
+import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
+import { DecimalPipe, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'design-system-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.scss'],
+  standalone: true,
+  imports: [CommonProgressBarModule, MatProgressSpinnerModule, DecimalPipe, NgForOf],
 })
 export class ProgressBarComponent {
-  public progressValue: number;
+  nbSteps = [2, 3, 4, 5];
+  step = 0;
+  percent = 0;
+
+  constructor() {
+    setInterval(() => this.step++, 1000);
+    setInterval(() => {
+      this.percent += Math.round(Math.random() * 10);
+      if (this.percent > 100) this.percent = 0;
+    }, 400);
+  }
 }
