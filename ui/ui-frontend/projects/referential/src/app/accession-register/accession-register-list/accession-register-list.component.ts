bfa71f69c061d2b425372228656c7ac8dbb1888e
Daniel Radeau <daniel.radeau.ext@culture.gouv.fr>
2024-10-21 11:00:07 +0200

    bugs #13122 fix(accession-register): facet details computing
    fixes:
    * sum facets for each loaded page
    * reset facets when criteria changes

--- ui/ui-frontend/projects/referential/src/app/accession-register/accession-register-list/accession-register-list.component.ts
+++ ui/ui-frontend/projects/referential/src/app/accession-register/accession-register-list/accession-register-list.component.ts
@@ -102,6 +102,7 @@ export class AccessionRegisterListComponent extends InfiniteScrollTable<Accessio
   searchRequest() {
     const query: AccessionRegisterSearchDto = this.constructQuery();
     const pageRequest = new PageRequest(0, DEFAULT_PAGE_SIZE, this.orderBy, this.direction, JSON.stringify(query));
+    this.accessionRegistersService.resetFacets();
     super.search(pageRequest);
   }
 
@@ -147,14 +148,13 @@ export class AccessionRegisterListComponent extends InfiniteScrollTable<Accessio
   }
 
   addCriteriaFromDateFilters(query: AccessionRegisterSearchDto) {
-    const dateInterval: { endDateMin: string; endDateMax: string } = this.accessionRegistersService.getDateIntervalChanges().getValue();
+    const dateInterval: {
+      endDateMin: string;
+      endDateMax: string;
+    } = this.accessionRegistersService.getDateIntervalChanges().getValue();
     if (dateInterval !== null && (dateInterval.endDateMin !== null || dateInterval.endDateMax !== null)) {
       query.endDateInterval = dateInterval;
     }
-  }
-
-  addCriteriaFromFilters(query: AccessionRegisterSearchDto) {
-    query.filters = this.filterMap;
   }
 
   addCriteriaFromSearch(query: AccessionRegisterSearchDto) {
