3a07954406c66d4e74915150db6758536eb494de
Alexandre <alexandre.paris.ext@culture.gouv.fr>
2024-12-10 15:38:18 +0100

    change for mongo with mongosh
     Veuillez saisir le message de validation pour vos modifications. Les lignes

--- tools/docker/mongo/replica-set/000_replicaset_dev.js
+++ tools/docker/mongo/replica-set/000_replicaset_dev.js
@@ -1,12 +1,1 @@
-rs.initiate({
-    "_id" : "rs0",
-    "members" : [
-        {
-            "_id" : 0,
-            "host" : "vitamui-mongo:27018",
-            "priority": 1,
-            "slaveDelay" : NumberLong(0),
-            "votes" : 1
-        }
-    ]
-});
+rs.initiate();
