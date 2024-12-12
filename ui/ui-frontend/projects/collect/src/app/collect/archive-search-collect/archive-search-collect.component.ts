4da3d785c875faa09620c7ffd7f4cf49ba35d3be
Hassane Diaby <hassane.diaby@smile.fr>
2024-12-12 11:36:34 +0100

    bug #13330 : collect breadcrumb

--- ui/ui-frontend/projects/collect/src/app/collect/archive-search-collect/archive-search-collect.component.ts
+++ ui/ui-frontend/projects/collect/src/app/collect/archive-search-collect/archive-search-collect.component.ts
@@ -36,7 +36,9 @@ import { debounceTime, map, mergeMap, tap } from 'rxjs/operators';
 import { isEmpty } from 'underscore';
 import {
   AccessContract,
+  ApplicationId,
   ArchiveSearchResultFacets,
+  BreadCrumbData,
   CriteriaDataType,
   CriteriaOperator,
   CriteriaSearchCriteria,
@@ -161,6 +163,7 @@ export class ArchiveSearchCollectComponent extends SidenavPage<any> implements O
 
   tenantIdentifier: string;
   projectName: string;
+  breadcrumbData: BreadCrumbData[];
 
   selectedArchive$: Observable<Unit>;
 
@@ -323,6 +326,11 @@ export class ArchiveSearchCollectComponent extends SidenavPage<any> implements O
     );
 
     this.checkUpdateUnitPermissions();
+    this.breadcrumbData = [
+      { identifier: ApplicationId.PORTAL_APP },
+      { identifier: ApplicationId.COLLECT_APP },
+      { label: this.projectName },
+    ];
   }
 
   private checkUpdateUnitPermissions() {
