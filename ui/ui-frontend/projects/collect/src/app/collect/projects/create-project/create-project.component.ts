5f93dca0d57360249dc579882106219682a8b10a
Etienne Bernard <ebernard@ippon.fr>
2025-01-15 15:12:18 +0100

    Bug #13152: Problem with re-displaying a directory after deletion and attempting to re-import it in the upload area

--- ui/ui-frontend/projects/collect/src/app/collect/projects/create-project/create-project.component.ts
+++ ui/ui-frontend/projects/collect/src/app/collect/projects/create-project/create-project.component.ts
@@ -234,6 +234,7 @@ export class CreateProjectComponent implements OnInit, OnDestroy, AfterViewCheck
       return;
     }
     await this.uploadService.handleUpload(items);
+    event.target.value = '';
   }
 
   addFolder() {
