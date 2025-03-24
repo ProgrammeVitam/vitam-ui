e0c82d3a26a010e2a213f77ac9763465b7c83022
Etienne Bernard <ebernard@ippon.fr>
2025-03-24 15:14:29 +0100

    Bugs #14276: fix CSV export to correctly quote fields

--- api/api-archive-search/archive-search/src/main/java/fr/gouv/vitamui/archives/search/server/service/ArchiveSearchUnitExportCsvService.java
+++ api/api-archive-search/archive-search/src/main/java/fr/gouv/vitamui/archives/search/server/service/ArchiveSearchUnitExportCsvService.java
@@ -176,7 +176,7 @@ public class ArchiveSearchUnitExportCsvService {
             // create a csv writer
             ICSVWriter csvWriter = new CSVWriterBuilder(writer)
                 .withSeparator(exportSearchResultParam.getSeparator())
-                .withQuoteChar(ICSVWriter.NO_QUOTE_CHARACTER)
+                .withQuoteChar(ICSVWriter.DEFAULT_QUOTE_CHARACTER)
                 .withEscapeChar(ICSVWriter.DEFAULT_ESCAPE_CHARACTER)
                 .withLineEnd(ICSVWriter.DEFAULT_LINE_END)
                 .build();
