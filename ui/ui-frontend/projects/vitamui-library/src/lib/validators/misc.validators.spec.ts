86a62474df1b5b3266608bf08b59607738237a84
Etienne Bernard <ebernard@ippon.fr>
2025-03-26 16:22:56 +0100

    Bugs #13226: validate required and non blank fields for groups and collect

---
+++ ui/ui-frontend/projects/vitamui-library/src/lib/validators/misc.validators.spec.ts
@@ -0,0 +1,73 @@
+/*
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2019-2022)
+ * and the signatories of the "VITAM - Accord du Contributeur" agreement.
+ *
+ * contact@programmevitam.fr
+ *
+ * This software is a computer program whose purpose is to implement
+ * implement a digital archiving front-office system for the secure and
+ * efficient high volumetry VITAM solution.
+ *
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
+import { TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MiscValidators } from './misc.validators';
+
+describe('MiscValidators', () => {
+  let formBuilder: FormBuilder;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
+
+    formBuilder = TestBed.inject(FormBuilder);
+  });
+
+  describe('requiredNotBlank', () => {
+    const expectValid = (str: string) => () => {
+      const formGroup = formBuilder.group({
+        field: [str, MiscValidators.requiredNotBlank],
+      });
+      expect(formGroup.valid).withContext('Form group must be valid').toBeTruthy();
+      expect(formGroup.get('field').errors).withContext('Field control must not have errors').toBeNull();
+    };
+    const expectInvalid = (str: string | null) => () => {
+      const formGroup = formBuilder.group({
+        field: [str, MiscValidators.requiredNotBlank],
+      });
+      expect(formGroup.invalid).withContext('Form group must be invalid').toBeTruthy();
+      expect(formGroup.get('field').errors).withContext('Field control must have errors').toBeTruthy();
+    };
+    it('should allow non blank field without spaces', expectValid('azerty'));
+    it('should allow non blank field with spaces', expectValid(' azer ty  '));
+    it('should not allow null fields', expectInvalid(null));
+    it('should not allow empty fields', expectInvalid(''));
+    it('should not allow blank fields', expectInvalid('    '));
+  });
+});
