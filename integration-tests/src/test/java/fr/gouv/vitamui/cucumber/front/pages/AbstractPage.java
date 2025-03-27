9eb47ff12c74860248975a2289dffc56a44995ad
Etienne Bernard <ebernard@ippon.fr>
2025-03-27 14:12:32 +0100

    Story #13791: [Clean code] remove integration tests (not working / not maintained)

--- integration-tests/src/test/java/fr/gouv/vitamui/cucumber/front/pages/AbstractPage.java
+++
@@ -1,5 +0,0 @@
-package fr.gouv.vitamui.cucumber.front.pages;
-
-import net.thucydides.core.pages.PageObject;
-
-public class AbstractPage extends PageObject {}
--- integration-tests/src/test/java/fr/gouv/vitamui/cucumber/front/utils/UserEnum.java
+++
@@ -1,7 +0,0 @@
-package fr.gouv.vitamui.cucumber.front.utils;
-
-public enum UserEnum {
-    ADMIN,
-    UNKNOW,
-    DEMO,
-}
