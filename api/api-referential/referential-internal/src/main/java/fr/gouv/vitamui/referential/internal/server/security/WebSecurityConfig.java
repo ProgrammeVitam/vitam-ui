59c508ac1bc0ad24dc6e89cc0499241a1bfb9f2f
Benaissa BENARBIA <benaissa.benarbia.ext@culture.gouv.fr>
2025-04-28 15:24:30 +0200

    Bug #14578: add controls to check the access to tenants

--- api/api-referential/referential-internal/src/main/java/fr/gouv/vitamui/referential/internal/server/security/WebSecurityConfig.java
+++ api/api-referential/referential-internal/src/main/java/fr/gouv/vitamui/referential/internal/server/security/WebSecurityConfig.java
@@ -38,6 +38,7 @@ package fr.gouv.vitamui.referential.internal.server.security;
 
 import fr.gouv.vitamui.commons.rest.RestExceptionHandler;
 import fr.gouv.vitamui.iam.security.config.InternalApiWebSecurityConfig;
+import fr.gouv.vitamui.iam.security.service.InternalSecurityService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -45,8 +46,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 /**
  * The security configuration.
- *
- *
  */
 
 @Configuration
@@ -56,8 +55,9 @@ public class WebSecurityConfig extends InternalApiWebSecurityConfig {
     public WebSecurityConfig(
         final AuthenticationProvider apiAuthenticationProvider,
         final RestExceptionHandler restExceptionHandler,
+        final InternalSecurityService internalSecurityService,
         final Environment env
     ) {
-        super(apiAuthenticationProvider, restExceptionHandler, env);
+        super(apiAuthenticationProvider, restExceptionHandler, internalSecurityService, env);
     }
 }
