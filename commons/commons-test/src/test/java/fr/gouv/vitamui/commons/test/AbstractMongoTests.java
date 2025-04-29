e183c62b77b2cb1c7e652b2b8ceed84932ea36a8
Julien Georges <julien.georges@culture.gouv.fr>
2025-04-29 11:04:01 +0200

    Story #14381: Update COTS to latest available versions before releasing.

--- commons/commons-test/src/test/java/fr/gouv/vitamui/commons/test/AbstractMongoTests.java
+++ commons/commons-test/src/test/java/fr/gouv/vitamui/commons/test/AbstractMongoTests.java
@@ -34,7 +34,7 @@ import org.testcontainers.containers.MongoDBContainer;
 
 public class AbstractMongoTests {
 
-    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0.6");
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0.8");
 
     @BeforeAll
     static void startContainers() {
