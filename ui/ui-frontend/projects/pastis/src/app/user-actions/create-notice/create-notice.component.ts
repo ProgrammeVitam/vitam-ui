6159d187d23da8e5040bea3e6de824fe0672e39d
Pierre-TROCHET <pierre.trochet.ext@culture.gouv.fr>
2025-05-19 16:39:57 +0200

    Bug #14316: fix archive profile update

--- ui/ui-frontend/projects/pastis/src/app/user-actions/create-notice/create-notice.component.ts
+++ ui/ui-frontend/projects/pastis/src/app/user-actions/create-notice/create-notice.component.ts
@@ -1,32 +1,59 @@
+/*
+ * Copyright French Prime minister Office/SGMAP/DINSIC/Vitam Program (2019-2022)
+ * and the signatories of the "VITAM - Accord du Contributeur" agreement.
+ *
+ * contact@programmevitam.fr
+ *
+ * This software is a computer program whose purpose is to implement
+ * implement a digital archiving front-office system for the secure and
+ * efficient high volumetry VITAM solution.
+ *
+ * This software is governed by the CeCILL-C license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL-C
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C license and that you accept its terms.
+ */
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog';
 import { Router } from '@angular/router';
-import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { ApplicationService } from 'vitamui-library';
-import { environment } from '../../../environments/environment';
 import { FileService } from '../../core/services/file.service';
-import { PopupService } from '../../core/services/popup.service';
 import { ProfileService } from '../../core/services/profile.service';
 import { ArchivalProfileUnit } from '../../models/archival-profile-unit';
 import { Notice } from '../../models/notice.model';
 import { Profile } from '../../models/profile';
 import { ProfileType } from '../../models/profile-type.enum';
-import { PastisDialogData } from '../../shared/pastis-dialog/classes/pastis-dialog-data';
 import { PastisDialogDataCreate } from '../save-profile/save-profile.component';
 import { ProfileVersion } from '../../models/profile-version.enum';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 interface Status {
   value: string;
   viewValue: string;
-}
-
-const POPUP_CREATION_CHOICE_PATH = 'PROFILE.POP_UP_CREATION_NOTICE.CHOICE';
-
-function constantToTranslate() {
-  this.profilActif = this.translated('.PROFIL_ACTIF');
-  this.profilInactif = this.translated('.PROFIL_INACTIF');
 }
 
 @Component({
@@ -34,42 +61,28 @@ function constantToTranslate() {
   selector: 'create-notice',
   templateUrl: './create-notice.component.html',
   styleUrls: ['./create-notice.component.scss'],
+  standalone: false,
 })
 export class CreateNoticeComponent implements OnInit, OnDestroy {
   form: FormGroup;
-  stepIndex = 0;
-  btnIsDisabled: boolean;
-  dialogData: PastisDialogData;
   notice: Notice;
   // edit or new notice
-  editNotice: boolean;
-  titleDialog: string;
-  subTitleDialog: string;
-  okLabel: string;
-  cancelLabel: string;
-  arrayStatus: Status[];
-  profileType?: ProfileType;
-  profileVersion?: ProfileVersion;
-  modePUA: boolean;
-  information: string;
-  presenceNonDeclareMetadonneesPUAControl = new FormControl(false);
-  createNotice: boolean;
-  profilActif: string;
-  profilInactif: string;
-  validate: boolean;
+  editNotice = false;
+  statusOptions: Status[];
+  profileType: ProfileType;
+  profileVersion: ProfileVersion;
+  modePUA = false;
   controlSchema: any;
-
-  isStandalone: boolean = environment.standalone;
 
   subscriptions = new Subscription();
   externalIdentifierEnabled: boolean;
+  public isIdentifierOK = true;
 
   constructor(
     public dialogRef: MatDialogRef<CreateNoticeComponent>,
     @Inject(MAT_DIALOG_DATA) public data: PastisDialogDataCreate,
     private formBuilder: FormBuilder,
     private translateService: TranslateService,
-    private popupService: PopupService,
     private fileService: FileService,
     private router: Router,
     private profileService: ProfileService,
@@ -82,14 +95,8 @@ export class CreateNoticeComponent implements OnInit, OnDestroy {
     if (this.profileType === ProfileType.PUA) {
       this.modePUA = true;
     }
-    this.applicationService
-      .isApplicationExternalIdentifierEnabled(this.profileType === ProfileType.PUA ? 'ARCHIVE_UNIT_PROFILE' : 'PROFILE')
-      .subscribe((value) => {
-        this.externalIdentifierEnabled = value;
-      });
     this.editNotice = this.router.url.substring(this.router.url.lastIndexOf('/') - 4, this.router.url.lastIndexOf('/')) === 'edit';
     if (this.editNotice) {
-      this.validate = true;
       // Subscribe observer to notice
       this.subscriptions.add(
         this.fileService.noticeEditable.subscribe((value: Notice) => {
@@ -104,130 +111,103 @@ export class CreateNoticeComponent implements OnInit, OnDestroy {
         identifier: '',
       };
     }
-
-    if (!this.isStandalone) {
-      constantToTranslate.call(this);
-      this.translatedOnChange();
-    } else if (this.isStandalone) {
-      this.profilActif = 'Profil actif';
-      this.profilInactif = 'Profil inactif';
-    }
-    this.arrayStatus = [
-      { value: 'INACTIVE', viewValue: this.profilInactif },
-      { value: 'ACTIVE', viewValue: this.profilActif },
+    this.statusOptions = [
+      { value: 'ACTIVE', viewValue: this.translateService.instant('PROFILE.POP_UP_CREATION_NOTICE.CHOICE.PROFIL_ACTIF') },
+      { value: 'INACTIVE', viewValue: this.translateService.instant('PROFILE.POP_UP_CREATION_NOTICE.CHOICE.PROFIL_INACTIF') },
     ];
-    this.information = "texte d'information";
-    this.controlSchema = JSON.parse(this.profileService.controlSchema.getValue());
+    if (this.profileType === ProfileType.PUA) {
+      this.controlSchema = JSON.parse(this.profileService.controlSchema.getValue());
+    }
     this.form = this.formBuilder.group({
-      identifier: [null, Validators.required],
-      intitule: [null, Validators.required],
-      selectedStatus: [null],
-      description: [null],
+      identifier: [{ value: this.notice.identifier, disabled: this.editNotice }, Validators.required],
+      name: [this.notice.name, Validators.required],
+      status: [this.notice.status],
+      description: [this.notice.description],
       additionalProperties: [this.controlSchema?.additionalProperties],
     });
 
-    this.subscriptions.add(
-      this.presenceNonDeclareMetadonneesPUAControl.valueChanges.subscribe((value) => {
-        this.form.controls.autoriserPresenceMetadonnees.setValue(value);
-      }),
-    );
+    this.applicationService
+      .isApplicationExternalIdentifierEnabled(this.profileType === ProfileType.PUA ? 'ARCHIVE_UNIT_PROFILE' : 'PROFILE')
+      .subscribe((value) => {
+        this.externalIdentifierEnabled = value;
+        if (this.editNotice || !this.externalIdentifierEnabled) {
+          this.form.controls.identifier.clearValidators();
+          this.form.controls.identifier.updateValueAndValidity();
+        }
+      });
 
-    // Subscribe observer to button status and
-    // set the inital state of the ok button to disabled
-
-    this.subscriptions.add(
-      this.popupService.btnYesShoudBeDisabled.subscribe((status) => {
-        this.btnIsDisabled = status;
-      }),
-    );
-  }
-
-  translatedOnChange(): void {
-    this.subscriptions.add(
-      this.translateService.onLangChange.subscribe((_: LangChangeEvent) => {
-        constantToTranslate.call(this);
-      }),
-    );
-  }
-
-  translated(nameOfFieldToTranslate: string): string {
-    return this.translateService.instant(POPUP_CREATION_CHOICE_PATH + nameOfFieldToTranslate);
+    if (!this.editNotice) {
+      this.subscriptions.add(
+        this.form.controls.identifier.valueChanges
+          .pipe(debounceTime(300), distinctUntilChanged())
+          .subscribe((identifier: string) => this.checkIdentifier(identifier)),
+      );
+    }
   }
 
   onCancel() {
     this.dialogRef.close();
   }
 
-  upateButtonStatusAndDataToSend() {
-    this.popupService.setPopUpDataOnClose('test');
-    this.popupService.disableYesButton(true);
-  }
-
   onNoClick(): void {
     this.dialogRef.close();
   }
 
-  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
 
-  checkIdentifier(modePUA: boolean) {
-    if (this.notice.identifier.length < 1) {
-      this.validate = false;
+  checkIdentifier(identifier: string) {
+    if (identifier.length === 0) {
       return;
     }
-    if (modePUA) {
-      const archivalProfileUnit = {} as ArchivalProfileUnit;
-      archivalProfileUnit.identifier = this.notice.identifier;
+    if (this.modePUA) {
+      const archivalProfileUnit = { identifier: identifier } as ArchivalProfileUnit;
       this.subscriptions.add(
         this.profileService.checkPuaProfile(archivalProfileUnit).subscribe((response: boolean) => {
           if (response) {
+            this.isIdentifierOK = false;
             alert('Identifier already exists use another identifier');
-            this.validate = false;
           } else {
-            this.validate = true;
-            this.checkIntitule();
+            this.isIdentifierOK = true;
           }
         }),
       );
     } else {
-      const profile = {} as Profile;
-      profile.identifier = this.notice.identifier;
+      const profile = { identifier: identifier } as Profile;
       this.subscriptions.add(
         this.profileService.checkPaProfile(profile).subscribe((response: boolean) => {
           if (response) {
+            this.isIdentifierOK = false;
             alert('Identifier already exists use another identifier');
-            this.validate = false;
           } else {
-            this.validate = true;
-            this.checkIntitule();
+            this.isIdentifierOK = true;
           }
         }),
       );
     }
   }
 
-  checkIntitule() {
-    this.validate = this.notice.name.length !== 0;
+  isFormValid(): boolean {
+    return this.form?.valid && this.isIdentifierOK;
   }
 
   onSubmit() {
-    if (!this.externalIdentifierEnabled) {
-      this.form.controls.identifier.setValue(this.form.controls.intitule.value);
+    if (!this.externalIdentifierEnabled && !this.editNotice) {
+      this.form.controls.identifier.setValue(this.form.controls.name.value);
     }
     if (this.form.invalid) {
       return;
     }
     if (this.editNotice) {
-      this.fileService.noticeEditable.next(this.notice);
+      this.fileService.noticeEditable.next(this.form.getRawValue());
       this.fileService.setNotice(true);
     }
     this.updateControlSchema();
     this.dialogRef.close({
       success: true,
       action: 'none',
-      data: this.form.value,
+      data: this.form.getRawValue(),
       profileType: this.profileType,
       profileVersion: this.profileVersion,
     });
