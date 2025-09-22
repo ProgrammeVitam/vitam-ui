7a199dcc4a16df8fd74456c36d7f8491d36351cb
Salim Terres <salim.terres.ext@culture.gouv.fr>
2025-09-22 17:32:43 +0200

    Bug #14713 - [Collect && SEARCH] - Filters not translated after switching to English.

--- ui/ui-frontend/projects/vitamui-library/src/app/modules/models/criteria/search-criteria.service.ts
+++ ui/ui-frontend/projects/vitamui-library/src/app/modules/models/criteria/search-criteria.service.ts
@@ -162,7 +162,7 @@ export class SearchCriteriaService {
         const defaultCriteriaConfig: Partial<SearchCriteriaAddAction> = {
           valueElt: { id: key, value: formattedValue, beginInterval: beginDate, endInterval: endDate },
           labelElt: formattedValue,
-          keyTranslated: categoryForKey !== SearchCriteriaTypeEnum.FIELDS,
+          keyTranslated: true,
           operator: operator,
           category: categoryForKey,
           dataType: dataType,
