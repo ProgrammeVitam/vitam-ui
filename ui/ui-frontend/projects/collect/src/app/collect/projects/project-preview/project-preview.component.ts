e7a01107d53d5a075cc536425abf3d18402d6ecd
Salim Terres <salim.terres.ext@culture.gouv.fr>
2025-10-17 17:06:21 +0200

    Bug #14756 - [Collect] Premature display of the confirmation message when editing a project.

--- ui/ui-frontend/projects/collect/src/app/collect/projects/project-preview/project-preview.component.ts
+++ ui/ui-frontend/projects/collect/src/app/collect/projects/project-preview/project-preview.component.ts
@@ -246,6 +246,7 @@ export class ProjectPreviewComponent implements OnInit, AfterViewInit, OnDestroy
   launchUpdate() {
     const dialogToOpen = this.confirmEditProject;
     this.selectedValue = 'YES';
+    this.transactions$.next(null);
     const pageRequest = new PageRequest(0, DEFAULT_PAGE_SIZE, 'id', Direction.ASCENDANT);
     this.projectApiService.getTransactionsByProjectId(pageRequest, this.projectId$.getValue()).subscribe((transactions) => {
       this.transactions$.next(transactions);
