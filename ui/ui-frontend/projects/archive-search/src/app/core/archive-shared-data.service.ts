30668d120d70cc15d01cdd59bc66c15a2e68622d
Salim Terres <salim.terres.ext@culture.gouv.fr>
2025-11-05 16:18:52 +0100

    Bug #15363 && Bug #15398 - [Search + Collect] When saving a search with multiple filters, only one filter is retained && [Search] Default filter "Archives with objects" gets unchecked.

--- ui/ui-frontend/projects/archive-search/src/app/core/archive-shared-data.service.ts
+++ ui/ui-frontend/projects/archive-search/src/app/core/archive-shared-data.service.ts
@@ -218,7 +218,9 @@ export class ArchiveSharedDataService {
       builder.addQueryParam(searchCriteria.valueElt.id, searchCriteria.valueElt.value);
       this.simpleSearchCriteriaAddSubject.next(searchCriteria);
     });
-    builder.navigate();
+
+    // Update URL with query params and create history entry
+    builder.navigate({ replaceUrl: false });
   }
 
   addSimpleSearchCriteriaSubject(searchCriteria: SearchCriteriaAddAction) {
@@ -281,7 +283,9 @@ export class ArchiveSharedDataService {
     const builder = this.queryParamsService.builder();
     builder.removeQueryParam(searchCriteriaAction.valueElt.id, searchCriteriaAction.valueElt.value);
     this.searchCriteriaRemoveFromChildSubject.next(searchCriteriaAction);
-    builder.navigate();
+
+    // Update URL with query params and create history entry
+    builder.navigate({ replaceUrl: false });
   }
 
   receiveRemoveFromChildSearchCriteriaSubject(): Observable<SearchCriteriaRemoveAction> {
