73324558e8dc4871f3d684601955dc13ca8e90e2
Julien Georges <julien.georges@culture.gouv.fr>
2026-03-19 11:39:04 +0100

    Bug #15730: Upgrading MongoDB to latest release available.
    * Updating to version 8.0.20.

--- api/api-iam/iam/src/test/java/fr/gouv/vitamui/iam/server/customer/service/InitCustomerServiceIntegrationTest.java
+++ api/api-iam/iam/src/test/java/fr/gouv/vitamui/iam/server/customer/service/InitCustomerServiceIntegrationTest.java
@@ -78,7 +78,7 @@ public class InitCustomerServiceIntegrationTest {
     private static class CommonRepositoriesConfig {}
 
     @Container
-    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0.17");
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0.20");
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
