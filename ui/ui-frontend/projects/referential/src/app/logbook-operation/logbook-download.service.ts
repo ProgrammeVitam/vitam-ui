e95acd8a2487148db70f6d3b660d936bce184dee
Hassane Diaby <hassane.diaby@smile.fr>
2026-08-17 14:39:55 +0200

    Bug #16747: Logbook : reports download issue
    (cherry picked from commit a430aa13df0267f483283836f7363619fca0db89)

--- ui/ui-frontend/projects/referential/src/app/logbook-operation/logbook-download.service.ts
+++ ui/ui-frontend/projects/referential/src/app/logbook-operation/logbook-download.service.ts
@@ -70,6 +70,7 @@ export class LogbookDownloadService extends SearchService<IEvent> {
     'IMPORT_PRESERVATION_SCENARIO',
     'IMPORT_GRIFFIN',
     'STP_IMPORT_GRIFFIN',
+    'STP_IMPORT_PRESERVATION_SCENARIO',
     'PRESERVATION',
     'INGEST_CLEANUP',
     'COLLECT_DELETION_ACTION',
@@ -147,6 +148,8 @@ export class LogbookDownloadService extends SearchService<IEvent> {
           case 'IMPORT_ONTOLOGY':
           case 'STP_REFERENTIAL_FORMAT_IMPORT':
           case 'IMPORT_GRIFFIN':
+          case 'STP_IMPORT_GRIFFIN':
+          case 'STP_IMPORT_PRESERVATION_SCENARIO':
           case 'IMPORT_PRESERVATION_SCENARIO':
             return DOWNLOAD_TYPE_REPORT;
           case 'HOLDINGSCHEME':
