477a1f07b73f51016104102a79874abfa42325b2
Maxime Robert <maxime.robert@smile.fr>
2026-08-18 14:28:01 +0200

    Story #16471: Clean code - remove barrel files

--- ui/ui-frontend/projects/referential/src/app/agency/agency-list/agency-list.component.ts
+++ ui/ui-frontend/projects/referential/src/app/agency/agency-list/agency-list.component.ts
@@ -43,15 +43,14 @@ import type { AdminUserProfile, Agency } from 'vitamui-library';
 import {
   AgencyService,
   ApplicationId,
+  DEFAULT_PAGE_SIZE,
   Direction,
   InfiniteScrollTable,
   PageRequest,
   Role,
   SecurityService,
-  DEFAULT_PAGE_SIZE,
   VitamUICommonModule,
 } from 'vitamui-library';
-import type { Agency, AdminUserProfile } from 'vitamui-library';
 import { AgencyCreateModule } from '../agency-create/agency-create.module';
 
 import { ImportDialogModule } from '../../shared/import-dialog/import-dialog.module';
