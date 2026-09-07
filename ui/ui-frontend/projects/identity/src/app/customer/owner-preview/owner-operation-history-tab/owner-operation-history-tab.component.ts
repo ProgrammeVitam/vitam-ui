775e9d17212b865c6d5d35673d7e636d6eeecbb3
Maxime Robert <maxime.robert@smile.fr>
2026-09-07 17:36:40 +0200

    Story #16471: Clean code - fix CI post rebase

--- ui/ui-frontend/projects/identity/src/app/customer/owner-preview/owner-operation-history-tab/owner-operation-history-tab.component.ts
+++ ui/ui-frontend/projects/identity/src/app/customer/owner-preview/owner-operation-history-tab/owner-operation-history-tab.component.ts
@@ -35,6 +35,7 @@
  * knowledge of the CeCILL-C license and that you accept its terms.
  */
 import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
+import type { HistoryEvent } from 'vitamui-library';
 import { AuthService, LogbookService, HistoryEventsComponent, CollapseComponent } from 'vitamui-library';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { TranslatePipe } from '@ngx-translate/core';
