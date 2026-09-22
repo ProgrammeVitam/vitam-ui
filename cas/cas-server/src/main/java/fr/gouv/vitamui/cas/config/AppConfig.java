77fe72c20a8633fd6f54ea31ca3e4ba09d3aab54
Lotfi GHERIBI <lotfi.gheribi.ext@culture.gouv.fr>
2026-09-22 11:34:44 +0200

    Story #16712: Make the service account used for IAM calls configurable
    IamApiDecorator hard-coded admin@change-it.fr as the identity sent to IAM
    when no user is authenticated. It is now read from
    vitamui.cas.service-account, defaulting to the previous value.

--- cas/cas-server/src/main/java/fr/gouv/vitamui/cas/config/AppConfig.java
+++ cas/cas-server/src/main/java/fr/gouv/vitamui/cas/config/AppConfig.java
@@ -339,8 +339,11 @@ public class AppConfig extends BaseTicketCatalogConfigurer {
     }
 
     @Bean
-    public IamApiDecorator iamApiDecorator(Utils utils) {
-        return new IamApiDecorator(utils);
+    public IamApiDecorator iamApiDecorator(
+        final Utils utils,
+        @Value("${vitamui.cas.service-account:admin@change-it.fr}") final String serviceAccount
+    ) {
+        return new IamApiDecorator(utils, serviceAccount);
     }
 
     @Bean
